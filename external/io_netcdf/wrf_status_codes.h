b565ff4f50be5c34944c3198e8137f9297fe90f5
John Michalakes <michalak@ucar.edu>
2004-05-17 16:59:14 +0000

    Added status codes for HDF5
    git-svn-id: https://svn-wrf-model.cgd.ucar.edu/trunk@203 b0b5d27b-6f0f-0410-a2a3-cb1e977edc3d

--- external/io_netcdf/wrf_status_codes.h
+++ external/io_netcdf/wrf_status_codes.h
@@ -59,3 +59,75 @@
   integer, parameter  :: WRF_WARN_LENGTH_LESS_THAN_1 = -1022    
   integer, parameter  :: WRF_WARN_MORE_DATA_IN_FILE  = -1023    
   integer, parameter  :: WRF_WARN_DATE_LT_LAST_DATE  = -1024
+
+! For HDF5 only
+  integer, parameter  :: WRF_HDF5_ERR_FILE                 = -200
+  integer, parameter  :: WRF_HDF5_ERR_MD                   = -201
+  integer, parameter  :: WRF_HDF5_ERR_TIME                 = -202
+  integer, parameter  :: WRF_HDF5_ERR_TIME_EOF             = -203
+  integer, parameter  :: WRF_HDF5_ERR_MORE_DATA_IN_FILE    = -204
+  integer, parameter  :: WRF_HDF5_ERR_DATE_LT_LAST_DATE    = -205
+  integer, parameter  :: WRF_HDF5_ERR_TOO_MANY_FILES       = -206
+  integer, parameter  :: WRF_HDF5_ERR_TYPE_MISMATCH        = -207
+  integer, parameter  :: WRF_HDF5_ERR_LENGTH_LESS_THAN_1   = -208
+  integer, parameter  :: WRF_HDF5_ERR_WRITE_RONLY_FILE     = -209
+  integer, parameter  :: WRF_HDF5_ERR_READ_WONLY_FILE      = -210
+  integer, parameter  :: WRF_HDF5_ERR_FILE_NOT_OPENED      = -211
+  integer, parameter  :: WRF_HDF5_ERR_DATESTR_ERROR        = -212
+  integer, parameter  :: WRF_HDF5_ERR_DRYRUN_READ          = -213
+  integer, parameter  :: WRF_HDF5_ERR_ZERO_LENGTH_GET      = -214
+  integer, parameter  :: WRF_HDF5_ERR_ZERO_LENGTH_PUT      = -215
+  integer, parameter  :: WRF_HDF5_ERR_2DRYRUNS_1VARIABLE   = -216
+  integer, parameter  :: WRF_HDF5_ERR_DATA_TYPE_NOTFOUND   = -217
+  integer, parameter  :: WRF_HDF5_ERR_READ_PAST_EOF        = -218
+  integer, parameter  :: WRF_HDF5_ERR_BAD_DATA_HANDLE      = -219
+  integer, parameter  :: WRF_HDF5_ERR_WRTLEN_NE_DRRUNLEN   = -220
+  integer, parameter  :: WRF_HDF5_ERR_DRYRUN_CLOSE         = -221
+  integer, parameter  :: WRF_HDF5_ERR_DATESTR_BAD_LENGTH   = -222
+  integer, parameter  :: WRF_HDF5_ERR_ZERO_LENGTH_READ     = -223
+  integer, parameter  :: WRF_HDF5_ERR_TOO_MANY_DIMS        = -224
+  integer, parameter  :: WRF_HDF5_ERR_TOO_MANY_VARIABLES   = -225
+  integer, parameter  :: WRF_HDF5_ERR_COUNT_TOO_LONG       = -226
+  integer, parameter  :: WRF_HDF5_ERR_DIMENSION_ERROR      = -227
+  integer, parameter  :: WRF_HDF5_ERR_BAD_MEMORYORDER      = -228
+  integer, parameter  :: WRF_HDF5_ERR_DIMNAME_REDEFINED    = -229
+  integer, parameter  :: WRF_HDF5_ERR_MD_AFTER_OPEN        = -230
+  integer, parameter  :: WRF_HDF5_ERR_CHARSTR_GT_LENDATA   = -231
+  integer, parameter  :: WRF_HDF5_ERR_BAD_DATA_TYPE        = -232
+  integer, parameter  :: WRF_HDF5_ERR_FILE_NOT_COMMITTED   = -233
+
+  integer, parameter  :: WRF_HDF5_ERR_ALLOCATION        = -2001
+  integer, parameter  :: WRF_HDF5_ERR_DEALLOCATION      = -2002
+  integer, parameter  :: WRF_HDF5_ERR_BAD_FILE_STATUS   = -2003
+  integer, parameter  :: WRF_HDF5_ERR_BAD_VARIABLE_DIM  = -2004
+  integer, parameter  :: WRF_HDF5_ERR_MDVAR_DIM_NOT_1D  = -2005
+  integer, parameter  :: WRF_HDF5_ERR_TOO_MANY_TIMES    = -2006
+  integer, parameter ::  WRF_HDF5_ERR_DATA_ID_NOTFOUND  = -2007
+
+  integer, parameter ::  WRF_HDF5_ERR_DATASPACE         = -300
+  integer, parameter ::  WRF_HDF5_ERR_DATATYPE          = -301
+  integer, parameter :: WRF_HDF5_ERR_PROPERTY_LIST      = -302
+
+  integer, parameter :: WRF_HDF5_ERR_DATASET_CREATE     = -303
+  integer, parameter :: WRF_HDF5_ERR_DATASET_READ       = -304
+  integer, parameter :: WRF_HDF5_ERR_DATASET_WRITE      = -305
+  integer, parameter :: WRF_HDF5_ERR_DATASET_OPEN       = -306
+  integer, parameter :: WRF_HDF5_ERR_DATASET_GENERAL    = -307
+  integer, parameter :: WRF_HDF5_ERR_GROUP              = -308
+
+  integer, parameter :: WRF_HDF5_ERR_FILE_OPEN          = -309
+  integer, parameter :: WRF_HDF5_ERR_FILE_CREATE        = -310
+  integer, parameter :: WRF_HDF5_ERR_DATASET_CLOSE      = -311
+  integer, parameter :: WRF_HDF5_ERR_FILE_CLOSE         = -312
+  integer, parameter :: WRF_HDF5_ERR_CLOSE_GENERAL      = -313
+
+  integer, parameter :: WRF_HDF5_ERR_ATTRIBUTE_CREATE   = -314
+  integer, parameter :: WRF_HDF5_ERR_ATTRIBUTE_READ     = -315
+  integer, parameter :: WRF_HDF5_ERR_ATTRIBUTE_WRITE    = -316
+  integer, parameter :: WRF_HDF5_ERR_ATTRIBUTE_OPEN     = -317
+  integer, parameter :: WRF_HDF5_ERR_ATTRIBUTE_GENERAL  = -318
+  integer, parameter :: WRF_HDF5_ERR_ATTRIBUTE_CLOSE    = -319
+
+  integer, parameter :: WRF_HDF5_ERR_OTHERS             = -320
+  integer, parameter :: WRF_HDF5_ERR_ATTRIBUTE_OTHERS   = -321
+

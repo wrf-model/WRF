74fcd08dbff14d558f39aa1b34b5f57775e801b1
John Michalakes <michalak@ucar.edu>
2004-12-23 17:31:20 +0000

    Suppress a type incompatibility message between char * and unsigned long (do a cast). JM
    git-svn-id: https://svn-wrf-model.cgd.ucar.edu/trunk@645 b0b5d27b-6f0f-0410-a2a3-cb1e977edc3d

--- external/RSL/RSL/message_def.c
+++ external/RSL/RSL/message_def.c
@@ -451,7 +451,7 @@ RSL_END_REGISTER_F90 ()
 RSL_START_REGISTER_F90 ()
 {
   base_table_cursor = 1 ;
-  f90_base_table[ 0 ].virt_base = BASE_TABLE_PADDING ;
+  f90_base_table[ 0 ].virt_base = (char *) BASE_TABLE_PADDING ;
   f90_base_table[ 0 ].size_in_bytes = 0 ;
 }
 

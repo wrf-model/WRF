370162d0dbc6dc5eb0c7ae887bdcffe60cfc80ac
John Michalakes <michalak@ucar.edu>
2005-06-03 03:32:33 +0000

    Bug fix when only 2 procs.  JM
    git-svn-id: https://svn-wrf-model.cgd.ucar.edu/trunk@1116 b0b5d27b-6f0f-0410-a2a3-cb1e977edc3d

--- external/RSL_LITE/c_code.c
+++ external/RSL_LITE/c_code.c
@@ -185,7 +185,7 @@ RSL_LITE_PACK ( char * buf , int * shw0 , int * typesize0 , int * xy0 , int * pu
 	if ( typesize == sizeof(int) ) {
 #ifdef crayx1
 /* PETER -- CHECK THIS AND UPDATE TO USE IMAX AND IMIN MACROS */
-        i4 = ips-shw ;
+        i4 = IMAX(ips-shw) ;
           i_offset = ipe+shw+1-i4;
           i3=0;
           for ( j = jpe-shw+1 ; j <= jpe ; j++ ) {
@@ -208,7 +208,7 @@ RSL_LITE_PACK ( char * buf , int * shw0 , int * typesize0 , int * xy0 , int * pu
           for ( j = jpe-shw+1 ; j <= jpe ; j++ ) {
             for ( k = kps ; k <= kpe ; k++ ) {
               pi = (int *)(p+yp_curs) ;
-              i = ips-shw ;
+              i = IMAX(ips-shw) ;
               qi = (int *)((buf + typesize*( (i-ims) + (ime-ims+1)*(
                                              (k-kms) + (j-jms)*(kme-kms+1))))) ;
               for ( i = IMAX(ips-shw) ; i <= IMIN(ipe+shw) ; i++ ) {
@@ -238,7 +238,7 @@ RSL_LITE_PACK ( char * buf , int * shw0 , int * typesize0 , int * xy0 , int * pu
 	if ( typesize == sizeof(int) ) {
 #ifdef crayx1
 /* PETER -- CHECK THIS AND UPDATE TO USE IMAX AND IMIN MACROS */
-        i4 = ips-shw ;
+        i4 = IMAX(ips-shw) ;
           i_offset = ipe+shw+1-i4;
           for ( j = jpe+1 ; j <= jpe+shw ; j++ ) {
           pi = (int *)(p+yp_curs) ;
@@ -260,7 +260,7 @@ RSL_LITE_PACK ( char * buf , int * shw0 , int * typesize0 , int * xy0 , int * pu
           for ( j = jpe+1 ; j <= jpe+shw ; j++ ) {
             for ( k = kps ; k <= kpe ; k++ ) {
               pi = (int *)(p+yp_curs) ;
-              i = ips-shw ;
+              i = IMAX(ips-shw) ;
               qi = (int *)((buf + typesize*( (i-ims) + (ime-ims+1)*(
                                              (k-kms) + (j-jms)*(kme-kms+1))))) ;
               for ( i = IMAX(ips-shw) ; i <= IMIN(ipe+shw) ; i++ ) {
@@ -300,7 +300,7 @@ RSL_LITE_PACK ( char * buf , int * shw0 , int * typesize0 , int * xy0 , int * pu
 	if ( typesize == sizeof(int) ) {
 #ifdef crayx1
 /* PETER -- CHECK THIS AND UPDATE TO USE IMAX AND IMIN MACROS */
-        i4 = ips-shw ;
+        i4 = IMAX(ips-shw) ;
           i_offset = ipe+shw+1-i4;
           for ( j = jps ; j <= jps+shw-1 ; j++ ) {
           pi = (int *)(p+ym_curs) ;
@@ -322,7 +322,7 @@ RSL_LITE_PACK ( char * buf , int * shw0 , int * typesize0 , int * xy0 , int * pu
           for ( j = jps ; j <= jps+shw-1 ; j++ ) {
             for ( k = kps ; k <= kpe ; k++ ) {
               pi = (int *)(p+ym_curs) ;
-              i = ips-shw ;
+              i = IMAX(ips-shw) ;
               qi = (int *)((buf + typesize*( (i-ims) + (ime-ims+1)*(
                                              (k-kms) + (j-jms)*(kme-kms+1))))) ;
               for ( i = IMAX(ips-shw) ; i <= IMIN(ipe+shw) ; i++ ) {
@@ -352,7 +352,7 @@ RSL_LITE_PACK ( char * buf , int * shw0 , int * typesize0 , int * xy0 , int * pu
 	if ( typesize == sizeof(int) ) {
 #ifdef crayx1
 /* PETER -- CHECK THIS AND UPDATE TO USE IMAX AND IMIN MACROS */
-        i4 = ips-shw ;
+        i4 = IMAX(ips-shw) ;
           i_offset = ipe+shw+1-i4;
           for ( j = jps-shw ; j <= jps-1 ; j++ ) {
           pi = (int *)(p+ym_curs) ;
@@ -375,7 +375,7 @@ RSL_LITE_PACK ( char * buf , int * shw0 , int * typesize0 , int * xy0 , int * pu
          for ( j = jps-shw ; j <= jps-1 ; j++ ) {
             for ( k = kps ; k <= kpe ; k++ ) {
               pi = (int *)(p+ym_curs) ;
-              i = ips-shw ;
+              i = IMAX(ips-shw) ;
               qi = (int *)((buf + typesize*( (i-ims) + (ime-ims+1)*(
                                              (k-kms) + (j-jms)*(kme-kms+1))))) ;
               for ( i = IMAX(ips-shw) ; i <= IMIN(ipe+shw) ; i++ ) {

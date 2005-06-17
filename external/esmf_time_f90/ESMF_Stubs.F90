0fdc89ef9082a283c083bb2f29067306c829130f
Tom Henderson <Thomas.B.Henderson@noaa.gov>
2005-06-17 20:34:39 +0000

    TBH:  Added stubs for LogErr.
    git-svn-id: https://svn-wrf-model.cgd.ucar.edu/trunk@1170 b0b5d27b-6f0f-0410-a2a3-cb1e977edc3d

--- external/esmf_time_f90/ESMF_Stubs.F90
+++ external/esmf_time_f90/ESMF_Stubs.F90
@@ -29,8 +29,25 @@ MODULE ESMF_Stubs
       INTEGER :: dummy
    END TYPE
 
+   TYPE ESMF_MsgType
+      SEQUENCE
+      INTEGER :: mtype
+   END TYPE
+   TYPE(ESMF_MsgType), PARAMETER  ::      &
+      ESMF_LOG_INFO  =   ESMF_MsgType(1), &
+      ESMF_LOG_WARNING = ESMF_MsgType(2), &
+      ESMF_LOG_ERROR =   ESMF_MsgType(3)
+
+   TYPE ESMF_LOG
+      SEQUENCE
+      INTEGER :: dummy
+   END TYPE
+
+
    PUBLIC ESMF_Grid, ESMF_GridComp, ESMF_State, ESMF_VM
-   PUBLIC ESMF_Initialize, ESMF_Finalize
+   PUBLIC ESMF_Initialize, ESMF_Finalize, ESMF_IsInitialized
+   PUBLIC ESMF_LogWrite, ESMF_LOG, ESMF_MsgType
+   PUBLIC ESMF_LOG_INFO, ESMF_LOG_WARNING, ESMF_LOG_ERROR
 
 CONTAINS
 
@@ -46,12 +63,32 @@ CONTAINS
    END SUBROUTINE ESMF_Initialize
 
 
+! NOOP for "is initialized" extension
+   FUNCTION ESMF_IsInitialized()
+      LOGICAL ESMF_IsInitialized
+      ESMF_IsInitialized = .TRUE.
+   END FUNCTION ESMF_IsInitialized
+
+
 ! NOOP
    SUBROUTINE ESMF_Finalize( rc )
       USE esmf_basemod
       INTEGER, INTENT(  OUT), OPTIONAL :: rc
       IF ( PRESENT( rc ) ) rc = ESMF_SUCCESS
    END SUBROUTINE ESMF_Finalize
+
+! NOOP
+   SUBROUTINE ESMF_LogWrite( msg, MsgType, line, file, method, log, rc )
+      USE esmf_basemod
+      CHARACTER(LEN=*), INTENT(IN) :: msg
+      TYPE(ESMF_MsgType), INTENT(IN) :: msgtype
+      INTEGER, INTENT(IN), OPTIONAL :: line
+      CHARACTER(LEN=*), INTENT(IN), OPTIONAL :: file
+      CHARACTER(LEN=*), INTENT(IN), OPTIONAL :: method
+      TYPE(ESMF_LOG),TARGET,OPTIONAL :: log
+      INTEGER, INTENT(OUT),OPTIONAL :: rc
+      IF ( PRESENT( rc ) ) rc = ESMF_SUCCESS
+   END SUBROUTINE ESMF_LogWrite
 
 
 END MODULE ESMF_Stubs

24135ab92c0e2ebac89b7343608e4ecda91b49f7
David Gill <gill@ucar.edu>
2005-07-26 12:52:11 +0000

    with Todd's new version of GriB1, these files are no longer required
    git-svn-id: https://svn-wrf-model.cgd.ucar.edu/trunk@1325 b0b5d27b-6f0f-0410-a2a3-cb1e977edc3d

--- external/io_grib1/bms.h
+++
@@ -1,8 +0,0 @@
-/* version 1.2 of grib headers  w. ebisuzaki */
-
-#define BMS_LEN(bms)		((bms) == NULL ? 0 : (bms[0]<<16)+(bms[1]<<8)+bms[2])
-#define BMS_UnusedBits(bms)	((bms) == NULL ? 0 : bms[3])
-#define BMS_StdMap(bms)		((bms) == NULL ? 0 : ((bms[4]<<8) + bms[5]))
-#define BMS_bitmap(bms)		((bms) == NULL ? NULL : (bms)+6)
-#define BMS_nxny(bms)		((((bms) == NULL) || BMS_StdMap(bms)) \
-	? 0 : (BMS_LEN(bms)*8 - 48 - BMS_UnusedBits(bms)))
--- external/io_grib1/test.F90
+++
@@ -1,6 +0,0 @@
-PROGRAM test
-  CHARACTER (len=21) :: teststr
-  teststr='abcdef'
-  print *,'teststr(1): ',teststr(1:1)
-
-END PROGRAM

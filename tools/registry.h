3c786f3557ed7e816751439512c2cd81a98fcb17
Tom Henderson <Thomas.B.Henderson@noaa.gov>
2006-05-08 16:54:36 +0000

    part of the initial new file set added for chemistry
    git-svn-id: https://svn-wrf-model.cgd.ucar.edu/trunk@1796 b0b5d27b-6f0f-0410-a2a3-cb1e977edc3d

--- tools/registry.h
+++ tools/registry.h
@@ -2,7 +2,8 @@
 #define NAMELEN 8092
 #define MAXDIMS 21
 #define MAX_DYNCORES 50   /* ha ha, just kidding */
-#define MAX_ARGLINE 128
+#define MAX_ARGLINE 175   /* WRF uses 128 by default, but the nested chem version hit the continuation line limit for efc so it had to be increased, wig 14-Oct-2004 */
+#define MAX_ARGLINE 128   /* welp, 175 means lines longer than 130 chars, which is a Fortran no no */
 #define MAX_TYPEDEFS 50   /* typedef history -ajb */
 
 /* defines of system commands */

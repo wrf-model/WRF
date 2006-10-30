6503a4a35290097d7c6a1b95c1fd6f0efb2aaec3
Tom Henderson <Thomas.B.Henderson@noaa.gov>
2006-10-30 19:11:17 +0000

    TBH:  - Add Cindy Bruyere's change to io_grib1 to return a more informative TBH:    error code from ext_gr1_read_field.  This has passed tests of TBH:    grib1 input and output in addition to regtest. TBH:  - Replace XTIME with a LOGICAL flag in phy_init() to avoid TBH:    potential problems due to floating-point comparison. TBH:  - Fix RSL_LITE+ESMF build to really fail in an easy-to-see way.  (This TBH:    is a bug left-over from last week.) TBH:  - Removed frame/protomain.F since no one has any use for it anymore.
    Changes are bitwise-exact vs. trunk_20061024_jm for regtest.csh.  
    
    
    
    git-svn-id: https://svn-wrf-model.cgd.ucar.edu/trunk@2026 b0b5d27b-6f0f-0410-a2a3-cb1e977edc3d

--- arch/Config.pl
+++ arch/Config.pl
@@ -295,15 +295,24 @@ close ARCH_POSTAMBLE ;
 close CONFIGURE_WRF ;
 
 # Die if attempting to configure with both RSL_LITE and a separately-installed ESMF library
-open CONFIGURE_WRF, "< configure.wrf" or die "cannot open configure.wrf" ;
-while ( <CONFIGURE_WRF> )
+if ( $sw_esmflib_path && $sw_esmfinc_path )
   {
-  if ( $_ =~ /DRSL_LITE/ )
+  my $RSL_LITE_plus_ESMF = "";
+  open CONFIGURE_WRF, "< configure.wrf" or die "cannot open configure.wrf for reading" ;
+  while ( <CONFIGURE_WRF> )
     {
-    die "cannot use a separately-installed ESMF library with RSL_LITE.  Please reconfigure to use RSL instead." ;
+    if ( $_ =~ /DRSL_LITE/ )
+      {
+      $RSL_LITE_plus_ESMF = "TRUE";
+      }
+    }
+  close CONFIGURE_WRF ;
+  if ( $RSL_LITE_plus_ESMF )
+    {
+    unlink("configure.wrf") ;
+    die "\nCONFIGURATION FAILED:  cannot use a separately-installed ESMF library with RSL_LITE.  Please reconfigure to use RSL instead.\n\n" ;
     }
   }
-close CONFIGURE_WRF ;
 
 printf "Configuration successful. To build the model type compile . \n" ;
 printf "------------------------------------------------------------------------\n" ;

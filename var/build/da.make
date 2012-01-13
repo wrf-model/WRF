wrf-model/WRF

commit fb5a0995576e37fcd3676370f3c1695e90da8bac

Fixes for FSO's forecast error respect to LBC treatment, adding adjoint of da_update_bc. Miscellaneous bug fixes for gpsref.
git-svn-id: https://svn-wrf-model.cgd.ucar.edu/trunk@5406 b0b5d27b-6f0f-0410-a2a3-cb1e977edc3d

var/build/da.make
@@ -148,9 +148,9 @@ WRFVAR_OBJS = \
 var : wrfvar
 var_esmf : wrfvar_esmf
 
-wrfvar : da_wrfvar.exe da_advance_time.exe da_update_bc.exe
+wrfvar : da_wrfvar.exe da_advance_time.exe da_update_bc.exe da_update_bc_ad.exe
 
-wrfvar_esmf : setup da_wrfvar_esmf.exe da_advance_time.exe da_update_bc.exe
+wrfvar_esmf : setup da_wrfvar_esmf.exe da_advance_time.exe da_update_bc.exe da_update_bc_ad.exe
 
 da_wrfvar.exe : $(WRF_SRC_ROOT_DIR)/frame/module_internal_header_util.o \
                 $(WRF_SRC_ROOT_DIR)/frame/pack_utils.o \
@@ -181,6 +181,7 @@ da_utils : \
            da_tune_obs_hollingsworth2.exe \
            da_tune_obs_desroziers.exe \
            da_update_bc.exe \
+           da_update_bc_ad.exe \
            da_advance_time.exe \
            da_verif_obs.exe \
            da_verif_grid.exe \
@@ -218,6 +219,12 @@ da_update_bc.exe : da_update_bc.o $(WRF_SRC_ROOT_DIR)/external/io_netcdf/libwrfi
 	$(SFC) $(LDFLAGS) -o $@ da_update_bc.o \
            da_netcdf_interface.o \
            da_module_couple_uv.o $(LIB_EXTERNAL)
+	@ if test -x $@ ;  then cd ../da; $(LN) ../build/$@ . ; fi
+
+da_update_bc_ad.exe : da_update_bc_ad.o $(WRF_SRC_ROOT_DIR)/external/io_netcdf/libwrfio_nf.a
+	$(SFC) $(LDFLAGS) -o $@ da_update_bc_ad.o \
+           da_netcdf_interface.o da_module_couple_uv.o \
+           da_module_couple_uv_ad.o $(LIB_EXTERNAL)
 	@ if test -x $@ ;  then cd ../da; $(LN) ../build/$@ . ; fi
 
 da_bias_airmass.exe : da_bias_airmass.o  rad_bias.o pythag.o tqli.o tred2.o regress_one.o
@@ -281,7 +288,7 @@ init_modules.o :
 da_bias_verif.o da_bias_scan.o da_bias_sele.o da_bias_airmass.o da_rad_diags.o \
 da_tune_obs_hollingsworth1.o da_tune_obs_hollingsworth2.o da_tune_obs_desroziers.o \
 da_verif_obs_control.o da_verif_obs_init.o da_verif_grid_control.o da_verif_obs.o \
-da_verif_grid.o :
+da_verif_grid.o da_update_bc.o da_update_bc_ad.o:
 	$(RM) $@
 	$(SED_FTN) $*.f90 > $*.b
 	$(CPP) $(CPPFLAGS) $(OMPCPP) $(FPPFLAGS) -I$(NETCDF)/include $*.b  > $*.f

var/da/da_gpsref/da_get_innov_vector_gpsref.inc
@@ -29,6 +29,12 @@ subroutine da_get_innov_vector_gpsref(it, num_qcstat_conv, grid, ob, iv)
    real    :: distance_h       ! cal. h-difference between obs and model
    real,allocatable :: min_dis(:)   ! minimum difference
                                                ! hor. location.
+! t_iwabuchi 20111216 linear-log interpolation for N
+   real, allocatable :: log_ref(:,:,:)   ! for log (N)
+
+! t_iwabuchi 20111216 T for QC 
+   real, allocatable :: int_t(:,:,:)     ! for T
+
    ! For quality control
 
    real   , parameter :: h_1 = 7000.0, h_2 = 25000.0
@@ -49,6 +55,12 @@ subroutine da_get_innov_vector_gpsref(it, num_qcstat_conv, grid, ob, iv)
    character(len=40) :: name_qc(5000)
    character(len=2) :: c_it
    character(len=7) :: c_nt
+
+! t_iwabuchii 20111216 GSI regional QC scheme
+   real :: g_height, g_lat, cutoff, stddev     ! QC cutoff GSI
+   integer, parameter :: qc_cutoff = -36
+   real, allocatable :: model_t(:,:)  ! Model value t at ob location
+
 
    if (trace_use_dull) call da_trace_entry("da_get_innov_vector_gpsref")
 
@@ -138,6 +150,11 @@ subroutine da_get_innov_vector_gpsref(it, num_qcstat_conv, grid, ob, iv)
 
    model_ref(:,:) = 0.0
 
+! t_iwabuchi 20111216 (hmn) allocate model_t for GSI regional QC
+   allocate (model_t(iv%info(gpsref)%max_lev,iv%info(gpsref)%n1:iv%info(gpsref)%n2))
+   model_t(:,:) = 0.0
+
+
    allocate (used_lev(kms:kme,iv%info(gpsref)%n1:iv%info(gpsref)%n2))
    used_lev(:,:) = missing_data 
 
@@ -209,9 +226,28 @@ subroutine da_get_innov_vector_gpsref(it, num_qcstat_conv, grid, ob, iv)
 
    call da_convert_zk (iv%info(gpsref))
 
-   call da_interp_lin_3d (grid%xb%ref, iv%info(gpsref), model_ref)
+! t_iwabuchi 20111216 (hmn's update) linear interpolation of log(n) 
+!   call da_interp_lin_3d (grid%xb%ref, iv%info(gpsref), model_ref)
+   allocate (log_ref(ims:ime, jms:jme, kms:kme))
+   log_ref = log(grid%xb%ref)
+   call da_interp_lin_3d (log_ref, iv%info(gpsref), model_ref)
+   deallocate(log_ref)
+   allocate (int_t(ims:ime, jms:jme, kms:kme))
+   int_t = grid%xb%t
+   call da_interp_lin_3d (int_t, iv%info(gpsref), model_t)
+   deallocate(int_t)
+! t_iwabuchi END 
+
 
    do n=iv%info(gpsref)%n1,iv%info(gpsref)%n2
+
+! t_iwabuchi 20111216 compute exp of log (N)
+   do k = 1, iv%info(gpsref)%levels(n)
+     model_ref(k,n)=exp(model_ref(k,n))
+!     model_t(k,n)=exp(model_t(k,n))
+   end do
+! t_iwabuchi END
+
       if ( (.not.(pseudo_var(1:3) == 'ref' .and. num_pseudo > 0)) .or. &
            it > 1 ) then
          do k = 1, iv%info(gpsref)%levels(n)
@@ -277,9 +313,13 @@ subroutine da_get_innov_vector_gpsref(it, num_qcstat_conv, grid, ob, iv)
                            ((iv%gpsref(n)%h(k+1)-iv%gpsref(n)%h(k-1))/1000.)
             end do
             do k=1, top_level
+! hmn 20111206
+               if (iv%gpsref(n)%ref(k)%qc /= missing_data) then
                if ((dndz_obs(k) < -50.) .or. (dndz_mod(k) < -50.)) then
                iv%gpsref(n)%ref(k)%qc = qc_step1
 !               print*,'QC_STEP1:',n,k,iv%gpsref(n)%h(k)/1000.,dndz_obs(k),dndz_mod(k)
+               end if
+! hmn 20111206 
                end if
             end do
 
@@ -303,10 +343,14 @@ subroutine da_get_innov_vector_gpsref(it, num_qcstat_conv, grid, ob, iv)
                             ((iv%gpsref(n)%h(k+1)-iv%gpsref(n)%h(k-1))/1000.)
             end do
             do k=1, top_level
+! hmn 20111206
+               if (iv%gpsref(n)%ref(k)%qc /= missing_data) then
                if ((abs(dndz2_obs(k)) > 100.) .or. (abs(dndz2_mod(k)) > 100.)) then
                iv%gpsref(n)%ref(k)%qc = qc_step2
 !               print*,'QC_STEP2:',n,k,iv%gpsref(n)%h(k)/1000.,dndz2_obs(k),dndz2_mod(k)
                end if
+               end if
+
             end do
           end if ! end of if pseudo check
 
@@ -317,6 +361,126 @@ subroutine da_get_innov_vector_gpsref(it, num_qcstat_conv, grid, ob, iv)
 !
 ! End of Poli's check. (2009) -------------------------------------------------
 ! 
+
+
+! t_iwabuchi 20111216 GSI's regional QC
+! START GSI-QC REGIONAL TI-GSI -------------------------------------------------
+! ------------------------------------------------------------------------------
+! GSI-QC Implementation, Ted Iwabuchi
+!   First release: 2011-03-04
+!   Review 3.3.1 : 2011-12-16
+!
+! HISTORY:
+!   2011-05-04 : bug in formula fixed
+!   2011-05-11 : add abs, and validated
+!   2011-06-06 : reviewd, added comments
+!   2011-12-16 : implemented for 3.3.1
+!
+! SUMMARY:
+! Modified version of Cucurull, 2010 for NCEP GSI-Global DA
+! NCEP-GSI regional DA  setupref.f90
+!
+!   O-B cutoff depending on assigned std dev for each height and latitude
+!
+!  > 30                                           all observation is rejected
+!  11 - 30 km    0.25 + 0.5 cos (lambda)   [c1]   3
+!   9 - 11 km    (11-h)/2 x c2 + (h-9)/2 x c1     3
+!   6 -  9 km    0.5 (if T<= 240)          [c2]   3
+!                0.01 x T^2 - 0.455 T + 52.075 (if T>240)
+!   4 -  6 km    (6-h)/2 x c3 + (h-4)/2 x c2    3
+!   0 -  4 km    1+2.5 cos (lambda)        [c3]   1
+!
+!   h, lambda, T
+! ------------------------------------------------------------------------------
+!
+! REFERENCES:
+!   GSI code: comGSI_v3Beta/src/main/setupref.f90
+!
+!
+! NOTES:
+!  See TI-GSI blocks, and don't forget to declare type of variables
+!  used in this block:
+!
+! 1  real :: g_height, g_lat, cutoff, stddev     ! QC cutoff GSI
+! 2  real, allocatable :: model_t(:,:)  ! Model value t at ob location
+! 3  integer, parameter :: qc_cutoff = -36
+!
+
+!  process refracticity data n from n1 to n2
+!
+   do n=iv%info(gpsref)%n1,iv%info(gpsref)%n2
+       do k=1,iv%info(gpsref)%levels(n)
+         if (model_ref(k,n) > 0.0) top_level=k
+
+         g_height = iv%gpsref(n)% h(k)
+         g_lat    = cos ( radian * iv%info(gpsref)%lat(k,n) )
+
+         if (     g_height >= 0.0     .and. g_height < 4000.0) then
+           cutoff = 1.0
+           stddev = 1.0 + 2.5 * g_lat
+
+         else if (g_height >= 4000.0  .and. g_height < 6000.0) then
+           cutoff = 3.0
+           if (model_t(k,n) <= 240.0) then
+             stddev = 0.5
+           else
+             stddev = 0.001 * model_t(k,n) * model_t(k,n) - 0.455 * model_t(k,n) + 52.075
+           endif
+           stddev = (6000.0 - g_height)/2000.0 * (1.0 + 2.5 * g_lat)  &
+                    + (g_height - 4000.0)/2000.0 * stddev
+
+         else if (g_height >= 6000.0  .and. g_height < 9000.0) then
+           cutoff = 3.0
+           if (model_t(k,n) <= 240.0) then
+             stddev = 0.5
+           else
+!            0.01 x T^2 - 0.455 T + 52.075 (if T>240)
+             stddev = 0.001 * model_t(k,n) * model_t(k,n) - 0.455 * model_t(k,n) + 52.075
+           end if
+
+         else if (g_height >= 9000.0  .and. g_height < 11000.0) then
+           cutoff = 3.0
+           if (model_t(k,n) <= 240.0) then
+             stddev = 0.5
+           else
+             stddev = 0.001 * model_t(k,n) * model_t(k,n) - 0.455 * model_t(k,n) + 52.075
+           endif
+           stddev = (11000.0 - g_height)/2000.0 * stddev  &
+                   + (g_height - 9000)/2000.0 * (0.25 + 0.5 * g_lat)
+
+
+         else if (g_height >= 11000.0 .and. g_height < 30000.0) then
+           cutoff = 3.0
+           stddev = 0.25 + 0.5 * g_lat
+
+         else if (g_height >= 30000.0 ) then
+           cutoff = 0.0
+
+         endif
+! hmn 20111206
+               if (iv%gpsref(n)%ref(k)%qc /= missing_data) then
+! Check innovation, stddev, and cutoff
+         if (abs(iv%gpsref(n)%ref(k)%inv) >= stddev * cutoff) then
+           iv%gpsref(n)%ref(k)%qc = qc_cutoff
+! hmn 20111202
+!           qc_36(k,n)=1
+! qc flag
+         endif
+! hmn 20111206
+               end if
+
+       end do
+   end do
+
+   deallocate (model_t)
+!
+! End   GSI-QC REGIONAL TI-GSI ------------------------------------------------
+! 
+! t_iwabuchi END 
+
+
+
+
    do n=iv%info(gpsref)%n1,iv%info(gpsref)%n2
       ! Quality check 2: Error percentage check.
 
@@ -331,7 +495,9 @@ subroutine da_get_innov_vector_gpsref(it, num_qcstat_conv, grid, ob, iv)
                percnt = 2.0 * abs(iv%gpsref(n)%ref(k)%inv / &
                  (ob%gpsref(n)%ref(k) + model_ref(k,n)))
 
-               if (iv%gpsref(n)%ref(k)%qc >= obs_qc_pointer) then
+! hmn 20111206
+!              if (iv%gpsref(n)%ref(k)%qc >= obs_qc_pointer) then
+               if (iv%gpsref(n)%ref(k)%qc /= missing_data) then
 
                   if (iv%gpsref(n)%h(k) < h_1) then
                      if (percnt > pcnt1) iv%gpsref(n)%ref(k)%qc = qc_below
@@ -381,8 +547,14 @@ subroutine da_get_innov_vector_gpsref(it, num_qcstat_conv, grid, ob, iv)
             do na = 1,nn
                if (iv%info(gpsref)%name(n) == name_qc(na)) then
                   do k=1, iv%info(gpsref)%levels(n)
+! hmn 20111202
                      if (iv%gpsref(n)%h(k) < height_below(na) .and. &
-                         iv%gpsref(n)%ref(k)%qc >= 0) iv%gpsref(n)%ref(k)%qc = qc_below
+!                        iv%gpsref(n)%ref(k)%qc >= 0) then
+! hmn 20111206
+                         iv%gpsref(n)%ref(k)%qc /= missing_data) then
+                       iv%gpsref(n)%ref(k)%qc = qc_below
+                     end if
+
                   end do
                   exit
                end if
@@ -401,7 +573,10 @@ subroutine da_get_innov_vector_gpsref(it, num_qcstat_conv, grid, ob, iv)
              if ( iv%gpsref(na)%ref(1)%qc == qc_below) then
                 do nb = iv%info(gpsref)%n1, iv%info(gpsref)%n2
                    if ( iv%info(gpsref)%id(nb) == iv%info(gpsref)%id(na) .and. &
-                        iv%gpsref(nb)%ref(1)%qc >= obs_qc_pointer .and.        &
+! hmn 20111206
+!                        iv%gpsref(nb)%ref(1)%qc >= obs_qc_pointer .and.        &
+                        iv%gpsref(n)%ref(1)%qc /= missing_data .and.            &
+
                         iv%gpsref(nb)%h(1) <= iv%gpsref(na)%h(1)   ) then
                       iv%gpsref(nb)%ref(1)%qc = qc_below
                    end if

var/da/da_update_bc/da_update_bc.f90
@@ -1378,6 +1378,10 @@ program da_update_bc
 
  end if ! end if update_lateral_bdy
 
+ deallocate(times)
+ deallocate(thisbdytime)
+ deallocate(nextbdytime)
+
  write(unit=stdout,fmt=*) &
     '=================================================================='
  if ( update_lateral_bdy ) then

var/da/da_gpsref/da_check_max_iv_gpsref.inc
@@ -5,6 +5,12 @@ subroutine da_check_max_iv_gpsref(iv,it, num_qcstat_conv, opt)
    ! Update:
    !    Removed Outerloop check as it is done in da_get_innov
    !    Author: Syed RH Rizvi,  MMM/NESL/NCAR,  Date: 07/12/2009
+   !
+   !    Added argument: opt = 1, max error check, and count the total num. of obs;
+   !                    opt = 2, only count the rejected num. of obs by all of QC
+   !                             procedures specified for GPSREF.
+   !        
+   !                             Shu-Ya Chen and Y.-R. Guo, 12/30/2011  
    !-----------------------------------------------------------------------
 
    implicit none
@@ -28,29 +34,29 @@ subroutine da_check_max_iv_gpsref(iv,it, num_qcstat_conv, opt)
    do n = iv%info(gpsref)%n1,iv%info(gpsref)%n2
       do k = 1, iv%info(gpsref)%levels(n)
         call da_get_print_lvl(iv%gpsref(n)%p(k)%inv,ipr)
+        if (iv%gpsref(n)%p(k)%inv == missing_r) ipr = 1
         failed=.false.
-        if( iv%gpsref(n)%ref(k)%qc >= obs_qc_pointer )  then
+        if( iv%gpsref(n)%ref(k)%qc >= obs_qc_pointer ) &
         call da_max_error_qc(it, iv%info(gpsref), n, iv%gpsref(n)%ref(k), max_error_ref, failed)  
-          if( iv%info(gpsref)%proc_domain(k,n) ) then
-                   num_qcstat_conv(1,gpsref,8,ipr) = num_qcstat_conv(1,gpsref,8,ipr) + 1
-          end if
-        end if
-
+        if( iv%info(gpsref)%proc_domain(k,n) ) &
+                 num_qcstat_conv(1,gpsref,8,ipr) = num_qcstat_conv(1,gpsref,8,ipr) + 1
       end do
    end do
          ENDIF
-
+  
          IF ( opt == 2 ) THEN
    do n = iv%info(gpsref)%n1,iv%info(gpsref)%n2
       do k = 1, iv%info(gpsref)%levels(n)
         call da_get_print_lvl(iv%gpsref(n)%p(k)%inv,ipr)
+        if (iv%gpsref(n)%p(k)%inv == missing_r) ipr = 1
         failed=.false.
         if ( ( iv%gpsref(n)%ref(k)%qc == fails_error_max ) .or. &
              ( iv%gpsref(n)%ref(k)%qc == qc_below ) .or. &
              ( iv%gpsref(n)%ref(k)%qc == qc_middle ) .or. &
              ( iv%gpsref(n)%ref(k)%qc == qc_above ) .or. &
              ( iv%gpsref(n)%ref(k)%qc == qc_step1 ) .or. &
-             ( iv%gpsref(n)%ref(k)%qc == qc_step2 ) ) then 
+             ( iv%gpsref(n)%ref(k)%qc == qc_step2 ) .or. &
+             ( iv%gpsref(n)%ref(k)%qc == missing_data ) ) then 
              failed=.true.
         endif
             if(failed) then

var/da/da_gpsref/da_gpsref.f90
@@ -9,7 +9,10 @@ module da_gpsref
       max_error_p,max_error_q, check_max_iv_unit,check_max_iv, qcstat_conv_unit, &
       max_stheight_diff,missing_data,max_error_bq,max_error_slp, ob_vars, &
       max_error_bt, max_error_buv, gpsref,max_error_thickness, &
-      pseudo_var, num_pseudo, kms,kme,kts,kte, trace_use_dull, &
+! t_iwabuchi 20121216 use ms,ime,jms,jme for interpolation of log(N)
+!      pseudo_var, num_pseudo, kms,kme,kts,kte, trace_use_dull, &
+      pseudo_var, num_pseudo, ims,ime,jms,jme, kms,kme,kts,kte, trace_use_dull, &
+! t_iwabuchi END
       anal_type_verify,fails_error_max,pseudo_err,pseudo_x, pseudo_y, stdout, &
       use_gpsrefobs, gpsref_thinning, pseudo_z,pseudo_val,max_error_ref, pseudo, &
       jts, jte,its,ite, npres_print, pptop

var/da/da_tools/da_get_print_lvl.inc
@@ -13,9 +13,10 @@ subroutine da_get_print_lvl(prs,ipr)
 
    if (trace_use) call da_trace_entry("da_get_print_lvl")
    ipr = 1
-   do k =2, npres_print 
-   if( prs*.01 .ge. pptop(k-1) ) return
-    ipr = k
+   do k =2, npres_print
+ ! Avoid the pressure is unavailable (ex. -8888.88) Shu-Ya Chen (Dec. 2011)
+     if( (prs*.01 .ge. pptop(k-1)) .or. (prs < 0.)) exit
+     ipr = k
    end do
 
    if (trace_use) call da_trace_exit("da_get_print_lvl")

var/da/da_update_bc/da_calc_mu_uv_ad.inc
@@ -1,0 +1,51 @@
+!        Generated by TAPENADE     (INRIA, Tropics team)
+!  Tapenade 3.6 (r4165) - 21 sep 2011 20:54
+!
+!  Differentiation of da_calc_mu_uv in reverse (adjoint) mode:
+!   gradient     of useful results: muu muv mu
+!   with respect to varying inputs: mu
+SUBROUTINE DA_CALC_MU_UV_B(mu, mub0, mub, muu, muub, muv, muvb, ids, ide&
+&  , jds, jde)
+  IMPLICIT NONE
+  INTEGER, INTENT(IN) :: ids, ide, jds, jde
+  REAL, DIMENSION(ids:ide, jds:jde), INTENT(IN) :: mu, mub
+  REAL, DIMENSION(ids:ide, jds:jde) :: mub0
+  REAL, DIMENSION(ids:ide + 1, jds:jde) :: muu
+  REAL, DIMENSION(ids:ide+1, jds:jde) :: muub
+  REAL, DIMENSION(ids:ide, jds:jde + 1) :: muv
+  REAL, DIMENSION(ids:ide, jds:jde+1) :: muvb
+  REAL, DIMENSION(ids - 1:ide + 1, jds - 1:jde + 1) :: mut
+  REAL, DIMENSION(ids-1:ide+1, jds-1:jde+1) :: mutb
+  INTEGER :: i, j
+  mutb = 0.0
+  DO j=jde+1,jds,-1
+    DO i=ide,ids,-1
+      mutb(i, j) = mutb(i, j) + 0.5*muvb(i, j)
+      mutb(i, j-1) = mutb(i, j-1) + 0.5*muvb(i, j)
+      muvb(i, j) = 0.0
+    END DO
+  END DO
+  DO j=jde,jds,-1
+    DO i=ide+1,ids,-1
+      mutb(i, j) = mutb(i, j) + 0.5*muub(i, j)
+      mutb(i-1, j) = mutb(i-1, j) + 0.5*muub(i, j)
+      muub(i, j) = 0.0
+    END DO
+  END DO
+  DO i=ide+1,ids-1,-1
+    mutb(i, jde) = mutb(i, jde) + mutb(i, jde+1)
+    mutb(i, jde+1) = 0.0
+    mutb(i, jds) = mutb(i, jds) + mutb(i, jds-1)
+    mutb(i, jds-1) = 0.0
+  END DO
+  DO j=jde,jds,-1
+    mutb(ide, j) = mutb(ide, j) + mutb(ide+1, j)
+    mutb(ide+1, j) = 0.0
+    mutb(ids, j) = mutb(ids, j) + mutb(ids-1, j)
+    mutb(ids-1, j) = 0.0
+    DO i=ide,ids,-1
+      mub0(i, j) = mub0(i, j) + mutb(i, j)
+      mutb(i, j) = 0.0
+    END DO
+  END DO
+END SUBROUTINE DA_CALC_MU_UV_B

var/da/da_update_bc/da_couple_ad.inc
@@ -1,0 +1,28 @@
+!        Generated by TAPENADE     (INRIA, Tropics team)
+!  Tapenade 3.6 (r4165) - 21 sep 2011 20:54
+!
+!  Differentiation of da_couple in reverse (adjoint) mode:
+!   gradient     of useful results: field
+!   with respect to varying inputs: field mut
+SUBROUTINE DA_COUPLE_B(mut, mutb, field, fieldb, msf, ids, ide, jds, jde&
+&  , kds, kde)
+  IMPLICIT NONE
+! if (trace_use) call da_trace_exit("da_couple")
+  INTEGER, INTENT(IN) :: ids, ide, jds, jde, kds, kde
+  REAL, INTENT(IN) :: mut(ids:ide, jds:jde)
+  REAL :: mutb(ids:ide, jds:jde)
+  REAL, INTENT(IN) :: msf(ids:ide, jds:jde)
+  REAL, INTENT(INOUT) :: field(ids:ide, jds:jde, kds:kde)
+  REAL, INTENT(INOUT) :: fieldb(ids:ide, jds:jde, kds:kde)
+  INTEGER :: i, j, k
+  mutb = 0.0
+  DO j=jde,jds,-1
+    DO k=kde,kds,-1
+      DO i=ide,ids,-1
+        mutb(i, j) = mutb(i, j) + field(i, j, k)*fieldb(i, j, k)/msf(i, &
+&          j)
+        fieldb(i, j, k) = mut(i, j)*fieldb(i, j, k)/msf(i, j)
+      END DO
+    END DO
+  END DO
+END SUBROUTINE DA_COUPLE_B

commit a76d566938dd4f8fd04bc9af8bd4e858ad77fa7d

1. lnetcdff might be needed to compile the recently modified da_verif_obs M       var/build/da.make
2. fix the bug introduced in r5406 commit
M       var/da/da_gpsref/da_get_innov_vector_gpsref.inc

3. somehow the fix makes the program run with NETCDF4
M       var/da/da_update_bc/da_update_bc.f90

4. correct the number of package temfpbl
M    Registry/Registry.wrfvar



git-svn-id: https://svn-wrf-model.cgd.ucar.edu/trunk@5439 b0b5d27b-6f0f-0410-a2a3-cb1e977edc3d

Registry/Registry.wrfvar
@@ -2551,7 +2551,7 @@ package   boulacscheme   bl_pbl_physics==8           -             state:el_pbl,
 package   camuwpblscheme bl_pbl_physics==9           -             state:tauresx2d,tauresy2d,qpert2d,wpert2d,tke_pbl
 package   surfdragscheme bl_pbl_physics==98          -             -
 package   mrfscheme      bl_pbl_physics==99          -             -
-package   temfpblscheme  bl_pbl_physics==88          -             state:te_temf,kh_temf,km_temf,shf_temf,qf_temf,uw_temf,vw_temf,wupd_temf,mf_temf,thup_temf,qlup_temf,qtup_temf,cf3d_temf,hd_temf,lcl_temf,hct_temf,cfm_temf
+package   temfpblscheme  bl_pbl_physics==10          -             state:te_temf,kh_temf,km_temf,shf_temf,qf_temf,uw_temf,vw_temf,wupd_temf,mf_temf,thup_temf,qlup_temf,qtup_temf,cf3d_temf,hd_temf,lcl_temf,hct_temf,cfm_temf
 
 package   kfetascheme    cu_physics==1               -             -
 package   bmjscheme      cu_physics==2               -             -

var/build/da.make
@@ -192,8 +192,8 @@ da_utils : \
            da_bias_verif.exe \
            da_rad_diags.exe
 
-da_verif_obs.exe : da_verif_obs.o da_verif_obs_control.o da_verif_obs_init.o da_verif_tools.o
-	$(SFC) $(LDFLAGS) -o $@ da_verif_obs.o da_verif_obs_control.o da_verif_obs_init.o da_verif_tools.o -L$(NETCDFPATH)/lib -lnetcdf
+da_verif_obs.exe : da_verif_obs.o da_verif_obs_control.o da_verif_obs_init.o da_verif_tools.o $(WRF_SRC_ROOT_DIR)/external/io_netcdf/libwrfio_nf.a
+	$(SFC) $(LDFLAGS) -o $@ da_verif_obs.o da_verif_obs_control.o da_verif_obs_init.o da_verif_tools.o $(LIB_EXTERNAL)
 	@ if test -x $@ ;  then cd ../da; $(LN) ../build/$@ . ; fi
 
 da_verif_grid.exe : da_verif_grid.o da_verif_grid_control.o da_netcdf_interface.o $(WRF_SRC_ROOT_DIR)/external/io_netcdf/libwrfio_nf.a

var/da/da_gpsref/da_get_innov_vector_gpsref.inc
@@ -575,7 +575,7 @@ subroutine da_get_innov_vector_gpsref(it, num_qcstat_conv, grid, ob, iv)
                    if ( iv%info(gpsref)%id(nb) == iv%info(gpsref)%id(na) .and. &
 ! hmn 20111206
 !                        iv%gpsref(nb)%ref(1)%qc >= obs_qc_pointer .and.        &
-                        iv%gpsref(n)%ref(1)%qc /= missing_data .and.            &
+                        iv%gpsref(nb)%ref(1)%qc /= missing_data .and.            &
 
                         iv%gpsref(nb)%h(1) <= iv%gpsref(na)%h(1)   ) then
                       iv%gpsref(nb)%ref(1)%qc = qc_below

var/da/da_update_bc/da_update_bc.f90
@@ -252,7 +252,7 @@ program da_update_bc
    east_end=0
    north_end=0
 
-   cdfid = ncopn(da_file, NCNOWRIT, io_status )
+   cdfid = ncopn(da_file, NCWRITE, io_status )
 
    ! For 2D variables
    ! Get mu, mub, msfu, and msfv
@@ -1375,12 +1375,11 @@ program da_update_bc
    deallocate(mub)
    deallocate(msfu)
    deallocate(msfv)
+   deallocate(times)
+   deallocate(thisbdytime)
+   deallocate(nextbdytime)
 
  end if ! end if update_lateral_bdy
-
- deallocate(times)
- deallocate(thisbdytime)
- deallocate(nextbdytime)
 
  write(unit=stdout,fmt=*) &
     '=================================================================='
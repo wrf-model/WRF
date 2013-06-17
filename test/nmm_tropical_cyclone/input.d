3d0932ea6711230272fa270a206009074261646d
Laurie Carson <carson@ucar.edu>
2013-06-17 15:58:22 +0000

    1. Idealized NMM-HWRF case: add example data files, README and code cleanup
    This commit adds some example idealized sounding files, updates
    the README and cleans up some print statements in the code.
    
    A    test/nmm_tropical_cyclone/input.d
    A    test/nmm_tropical_cyclone/sigma.d
    A    test/nmm_tropical_cyclone/storm.center
    A    test/nmm_tropical_cyclone/namelist.wps
    A    test/nmm_tropical_cyclone/sound_gfdl.d
    A    test/nmm_tropical_cyclone/sound_jordan.d
    A    test/nmm_tropical_cyclone/sound_wet.d
    A    test/nmm_tropical_cyclone/sound.d
    A    test/nmm_tropical_cyclone/namelist.input
    A    test/nmm_tropical_cyclone/README.NMM.TROPICAL_CYCLONE
    M    dyn_nmm/module_initialize_tropical_cyclone.F
    
    
    
    git-svn-id: https://svn-wrf-model.cgd.ucar.edu/trunk@6723 b0b5d27b-6f0f-0410-a2a3-cb1e977edc3d

---
+++ test/nmm_tropical_cyclone/input.d
@@ -0,0 +1,4 @@
+0------------indx=0 on f-plane, 1 on beta-plane
+ 0-----------nenv: index for environ.
+ 20.0--------vm0: maximum tangential wind of the initial vortex (m/s)
+ 90.0--------rm0: radius of maximum wind of the initial vortex (km)

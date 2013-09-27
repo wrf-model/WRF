e7057c0cabc87e1b071cb5945b04e4426c4a7c4d
Jamie Bresch <hclin@ucar.edu>
2013-09-27 16:47:55 +0000

    Add SEVIRI (Spinning Enhanced Visible and InfraRed Imager) radiance data assimilation capability. For now, InfraRed channels and CRTM only. Contribution from Hongli Wang and Hans Huang.
    M    var/build/depend.txt
    M    var/da/da_setup_structures/da_setup_obs_structures.inc
    M    var/da/da_setup_structures/da_setup_structures.f90
    M    var/da/da_test/da_check_xtoy_adjoint.inc
    A    var/da/da_radiance/da_read_obs_bufrseviri.inc
    M    var/da/da_radiance/da_radiance.f90
    M    var/da/da_radiance/da_radiance1.f90
    A    var/da/da_radiance/da_qc_seviri.inc
    M    var/da/da_radiance/da_qc_rad.inc
    M    var/da/da_radiance/da_read_pseudo_rad.inc
    M    var/da/da_radiance/da_radiance_init.inc
    M    var/da/da_radiance/module_radiance.f90
    M    var/da/da_radiance/da_setup_radiance_structures.inc
    A    var/run/radiance_info/meteosat-9-seviri.info
    M    var/run/VARBC.in
    AM   var/run/crtm_coeffs/seviri_m08.TauCoeff.bin
    AM   var/run/crtm_coeffs/seviri_m08.SpcCoeff.bin
    AM   var/run/crtm_coeffs/seviri_m09.TauCoeff.bin
    AM   var/run/crtm_coeffs/seviri_m10.TauCoeff.bin
    AM   var/run/crtm_coeffs/seviri_m09.SpcCoeff.bin
    AM   var/run/crtm_coeffs/ODPS/seviri_m10.TauCoeff.bin
    AM   var/run/crtm_coeffs/ODPS/seviri_m08.TauCoeff.bin
    AM   var/run/crtm_coeffs/ODPS/seviri_m09.TauCoeff.bin
    AM   var/run/crtm_coeffs/seviri_m10.SpcCoeff.bin
    AM   var/run/crtm_coeffs/ODAS/seviri_m10.TauCoeff.bin
    AM   var/run/crtm_coeffs/ODAS/seviri_m08.TauCoeff.bin
    AM   var/run/crtm_coeffs/ODAS/seviri_m09.TauCoeff.bin
    M    Registry/registry.var
    
    
    git-svn-id: https://svn-wrf-model.cgd.ucar.edu/trunk@6873 b0b5d27b-6f0f-0410-a2a3-cb1e977edc3d

--- var/da/da_setup_structures/da_setup_structures.f90
+++ var/da/da_setup_structures/da_setup_structures.f90
@@ -58,7 +58,8 @@ module da_setup_structures
       fmt_info, fmt_srfc, fmt_each, unit_end, max_ext_its, &  
       psi_chi_factor, psi_t_factor, psi_ps_factor, psi_rh_factor, &
       chi_u_t_factor, chi_u_ps_factor,chi_u_rh_factor, t_u_rh_factor, ps_u_rh_factor, &
-      interpolate_stats, be_eta, thin_rainobs, fgat_rain_flags, use_iasiobs
+      interpolate_stats, be_eta, thin_rainobs, fgat_rain_flags, use_iasiobs, &
+      use_seviriobs 
 
    use da_obs, only : da_fill_obs_structures, da_store_obs_grid_info, da_store_obs_grid_info_bufr, &
                       da_fill_obs_structures_rain

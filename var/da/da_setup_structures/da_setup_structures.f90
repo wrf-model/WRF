a969f90532c1afaec91a7278d28a656f06ecc914
Michael Kavulich <kavulich@ucar.edu>
2014-02-28 17:43:21 +0000

    Because of the observation thinning changes, the names of the following two files make no sense anymore: "da_store_obs_grid_info_bufr.inc" is called for PREPBUFR and ASCII observations, while "da_store_obs_grid_info.inc" is called for radiance observations, which are in BUFR format.
    To head off any future confusion, I'm renaming "da_store_obs_grid_info_bufr.inc" to "da_store_obs_grid_info.inc", and renaming the file formerly known as "da_store_obs_grid_info.inc" to "da_store_obs_grid_info_rad.inc"
    
    
    
    
    git-svn-id: https://svn-wrf-model.cgd.ucar.edu/trunk@7221 b0b5d27b-6f0f-0410-a2a3-cb1e977edc3d

--- var/da/da_setup_structures/da_setup_structures.f90
+++ var/da/da_setup_structures/da_setup_structures.f90
@@ -61,7 +61,7 @@ module da_setup_structures
       interpolate_stats, be_eta, thin_rainobs, fgat_rain_flags, use_iasiobs, &
       use_seviriobs,jds_int,jde_int,anal_type_hybrid_dual_res 
 
-   use da_obs, only : da_fill_obs_structures, da_store_obs_grid_info, da_store_obs_grid_info_bufr, &
+   use da_obs, only : da_fill_obs_structures, da_store_obs_grid_info, da_store_obs_grid_info_rad, &
                       da_fill_obs_structures_rain,da_set_obs_missing,da_set_3d_obs_missing
    use da_obs_io, only : da_read_obs_bufr,da_read_obs_radar, &
       da_scan_obs_radar,da_scan_obs_ascii,da_read_obs_ascii, &

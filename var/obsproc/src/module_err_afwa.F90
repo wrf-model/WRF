wrf-model/WRF

commit dc90c564b4c358af40b0ef214bb47d1cd0d7f1b9

- Replacing a whole bunch of "stop" and "print" statements, with calls to da_error, da_warning, or da_message. Good riddance.
M       WRFDA/var/da/da_vtox_transforms/da_add_flow_dependence_xa_adj_dual_res.inc
M       WRFDA/var/da/da_gen_be/da_readwrite_be_stage4.inc
M       WRFDA/var/da/da_gen_be/da_gen_be.f90
M       WRFDA/var/da/da_obs/da_obs_proc_station.inc
M       WRFDA/var/da/da_interpolation/da_interpolation.f90
M       WRFDA/var/da/da_interpolation/da_interp_2d_partial.inc
M       WRFDA/var/da/da_sound/da_check_buddy_sound.inc
M       WRFDA/var/da/da_radar/da_max_error_qc_radar.inc
M       WRFDA/var/da/da_radar/da_write_oa_radar_ascii.inc
M       WRFDA/var/da/da_define_structures/da_allocate_background_errors.inc
M       WRFDA/var/da/da_define_structures/da_random_seed.inc
M       WRFDA/var/da/da_define_structures/da_define_structures.f90
M       WRFDA/var/da/da_define_structures/da_initialize_cv.inc
M       WRFDA/var/da/da_setup_structures/da_setup_obs_interp_wts.inc
M       WRFDA/var/da/da_setup_structures/da_setup_be_regional.inc
M       WRFDA/var/da/da_setup_structures/da_setup_be_nmm_regional.inc
M       WRFDA/var/da/da_setup_structures/da_chg_be_Vres.inc
M       WRFDA/var/da/da_setup_structures/da_setup_be_ncep_gfs.inc
M       WRFDA/var/da/da_setup_structures/da_setup_structures.f90
M       WRFDA/var/da/da_test/da_transform_xtovp.inc
M       WRFDA/var/da/da_main/da_wrfvar_init2.inc
M       WRFDA/var/da/da_main/da_solve.inc
M       WRFDA/var/da/da_gpsref/da_get_innov_vector_gpsref.inc
M       WRFDA/var/da/da_pseudo/da_transform_xtoy_pseudo_adj.inc
M       WRFDA/var/da/da_gpspw/da_get_innov_vector_gpsztd.inc
M       WRFDA/var/da/da_gpspw/da_get_innov_vector_gpspw.inc
M       WRFDA/var/da/da_radiance/da_cloud_detect_airs.inc
M       WRFDA/var/da/da_radiance/da_cloud_sim.inc
M       WRFDA/var/da/da_radiance/da_read_obs_bufrseviri.inc
M       WRFDA/var/da/da_radiance/da_read_obs_fy3.inc
M       WRFDA/var/da/da_radiance/da_read_obs_bufriasi.inc

var/da/da_gpsref/da_get_innov_vector_gpsref.inc
@@ -275,7 +275,6 @@ subroutine da_get_innov_vector_gpsref(it, num_qcstat_conv, grid, ob, iv)
       do k=1,iv%info(gpsref)%levels(n)
          if (model_ref(k,n) > 0.0) top_level=k
       end do
-!     print*,'top_level=',top_level
       allocate(dndz_obs(top_level))
       allocate(dndz_mod(top_level))
       allocate(dndz2_obs(top_level))
@@ -310,7 +309,6 @@ subroutine da_get_innov_vector_gpsref(it, num_qcstat_conv, grid, ob, iv)
                if (iv%gpsref(n)%ref(k)%qc /= missing_data) then
                if ((dndz_obs(k) < -50.) .or. (dndz_mod(k) < -50.)) then
                iv%gpsref(n)%ref(k)%qc = qc_step1
-!               print*,'QC_STEP1:',n,k,iv%gpsref(n)%h(k)/1000.,dndz_obs(k),dndz_mod(k)
                end if
 ! hmn 20111206 
                end if
@@ -340,7 +338,6 @@ subroutine da_get_innov_vector_gpsref(it, num_qcstat_conv, grid, ob, iv)
                if (iv%gpsref(n)%ref(k)%qc /= missing_data) then
                if ((abs(dndz2_obs(k)) > 100.) .or. (abs(dndz2_mod(k)) > 100.)) then
                iv%gpsref(n)%ref(k)%qc = qc_step2
-!               print*,'QC_STEP2:',n,k,iv%gpsref(n)%h(k)/1000.,dndz2_obs(k),dndz2_mod(k)
                end if
                end if
 

commit 320ac0ccbaffc6120d09dccc76c037474d78f1a0

1. fixes for seviri  1) make the code handle seviri data from meteosat-9 and meteosat-10  2) correct the counts shown in qcstat file  NOTE for the instrument triplets specification for seviri in the  namelist.input: if we stick to Table 2 of RTTOV Users Guide for  platform_id and satellite_id, meteosat-9 (3/9) should be msg-2 (12/2)  and meteosat-10 (3/10) should be msg-3 (12/3).  For now, we go for (3/9) and (3/10). M       var/da/da_radiance/da_read_obs_bufrseviri.inc A       var/run/radiance_info/meteosat-10-seviri.info M       var/run/VARBC.in M       var/da/da_radiance/da_qc_seviri.inc
2. fix for obs counting printout for eos-2-airs and eos-2-amsua (long
   existing bugs that never got fixed)
M       var/da/da_radiance/da_read_obs_bufrairs.inc

3. fix for obs counting printout (problems introduced after thin_conv
   implemented for ob.ascii data stream)
M       var/da/da_setup_structures/da_setup_obs_structures.inc
M       var/da/da_obs_io/da_read_obs_bufrgpsro.inc

4. q variable missed for airep
M       var/da/da_obs/da_set_obs_missing.inc

5. reformat a little bit for obsproc's reformatted observation errors
   files
M       var/obsproc/src/module_err_afwa.F90

6. fix a printout bug introduced in r7225.
M       var/da/da_setup_structures/da_setup_be_regional.inc

7. fix for gpsref low-level quality check when gpsref data are not
   processed as profiles
M       var/da/da_obs_io/da_read_obs_bufrgpsro.inc
M       var/da/da_gpsref/da_get_innov_vector_gpsref.inc



git-svn-id: https://svn-wrf-model.cgd.ucar.edu/trunk@7288 b0b5d27b-6f0f-0410-a2a3-cb1e977edc3d

var/da/da_gpsref/da_get_innov_vector_gpsref.inc
@@ -563,6 +563,7 @@ subroutine da_get_innov_vector_gpsref(it, num_qcstat_conv, grid, ob, iv)
              if ( iv%gpsref(na)%ref(1)%qc == qc_below) then
                 do nb = iv%info(gpsref)%n1, iv%info(gpsref)%n2
                    if ( iv%info(gpsref)%id(nb) == iv%info(gpsref)%id(na) .and. &
+                        iv%info(gpsref)%name(nb) == iv%info(gpsref)%name(na) .and. &
 ! hmn 20111206
 !                        iv%gpsref(nb)%ref(1)%qc >= obs_qc_pointer .and.        &
                         iv%gpsref(nb)%ref(1)%qc /= missing_data .and.            &

var/da/da_obs/da_set_obs_missing.inc
@@ -84,6 +84,7 @@ subroutine da_set_obs_missing (iv, n)
             iv%airep(i)%u(:) = field_type(missing_r, missing_data, xmiss, missing_r, missing_r)
             iv%airep(i)%v(:) = field_type(missing_r, missing_data, xmiss, missing_r, missing_r)
             iv%airep(i)%t(:) = field_type(missing_r, missing_data, xmiss, missing_r, missing_r)
+            iv%airep(i)%q(:) = field_type(missing_r, missing_data, xmiss, missing_r, missing_r)
          end if
       end do
    case(gpspw)

var/da/da_obs_io/da_read_obs_bufrgpsro.inc
@@ -57,9 +57,11 @@ subroutine da_read_obs_bufrgpsro (iv)
        type(datalink_gpsro), pointer :: next
    end type datalink_gpsro
    type(datalink_gpsro),pointer  :: head=>null(), plink=>null()
+   integer(i_kind)      :: iprof
 
    if (trace_use) call da_trace_entry("da_read_obs_bufrgosro")
 
+   iprof  = 0
    nlocal = 0
    ntotal = 0
    num_report       = 0
@@ -181,7 +183,9 @@ bufrfile:  do ibufr=1,numbufr
             if (obs_time >= time_slots(ifgat-1) .and.  &
                 obs_time  < time_slots(ifgat)) exit
          end do  
-         
+
+      iprof = iprof + 1
+
       lev_loop: do k = 1, nlev1
 
          info%lat  = rdata1(1,k)
@@ -279,14 +283,15 @@ bufrfile:  do ibufr=1,numbufr
             end if
          end if
 
-         write(info%name, '(a,i6.6,a,a)') 'NCEP_GPSRO_', nlocal, '_', date_char
+         !write(info%name, '(a,i6.6,a,a)') 'NCEP_GPSRO_', nlocal, '_', date_char
+         write(info%name, '(a,i6.6,a,a)') 'NCEP_GPSRO_', iprof, '_', date_char
 
          if ( print_detail_obs ) then
             write(unit=stdout,fmt='(a,1x,a,1x,i4.4,4i2.2,2f8.2,f8.1,f8.2,i3,f9.5)')  &
                info%name,id(1:5),iyear,imonth,iday,ihour,imin, &
                info%lat,info%lon,height,ref_data,ref_qc,ref_error 
          end if
-         
+        
          if (.not. associated(head)) then
              nullify ( head )
              allocate ( head )
@@ -467,6 +472,9 @@ end do bufrfile
       ntotal_ifgat(kk)=ntotal_ifgat(kk)+ntotal_ifgat(kk-1)
       iv%info(gpsref)%ptotal(kk)=ntotal_ifgat(kk)
       iv%info(gpsref)%plocal(kk)=nlocal
+      ! thinning is not applied to GPSRO BUFR, simply make a copy from total number
+      iv%info(gpsref)%thin_ptotal(kk)=ntotal_ifgat(kk)
+      iv%info(gpsref)%thin_plocal(kk)=nlocal
    end do 
 
    write(unit=message(1),fmt='(A,3(1x,i7))') &

var/da/da_radiance/da_qc_seviri.inc
@@ -87,6 +87,17 @@ subroutine da_qc_seviri (it, i, nchan, ob, iv)
                   if (iv%instid(i)%info%proc_domain(1,n)) &
                   nrej_omb_std(k) = nrej_omb_std(k) + 1
                end if
+
+               ! final QC decsion
+               if (iv%instid(i)%tb_qc(k,n) == qc_bad) then
+                  iv%instid(i)%tb_error(k,n) = 500.0
+                  if (iv%instid(i)%info%proc_domain(1,n)) &
+                     nrej(k) = nrej(k) + 1
+               else
+                  if (iv%instid(i)%info%proc_domain(1,n)) &
+                     ngood(k) = ngood(k) + 1
+               end if
+
             end if   ! qc_good
          end do      ! nchan
       end do ! end loop pixel

var/da/da_radiance/da_read_obs_bufrairs.inc
@@ -113,13 +113,14 @@ subroutine da_read_obs_bufrairs(obstype,iv,infile)
 
 
 ! Other work variables
-  integer(i_kind)  :: nreal, nobs(0:num_fgat_time), ityp,ityp2
+  integer(i_kind)  :: nreal
   integer(i_kind)  :: k, iobsout
   integer(i_kind),dimension(19)::icount
   real(r_kind)     :: rlat, rlon, dx, dy, dx1, dy1, sstx, dlon, dlat
   real(r_kind)     :: sza, timedif, pred, crit1
   integer(i_kind)  :: klat1, klon1, klatp1, klonp1
   integer(i_kind)  :: ifov,size
+  integer(i_kind)  :: num_eos_file, num_eos_global, num_eos_local, num_eos_kept, num_eos_thinned
   integer(i_kind)  :: inst,platform_id,satellite_id,sensor_id
   logical          :: iflag,outside,outside_all
   integer(i_kind)  :: i, l, n, error, airs_table_unit
@@ -128,7 +129,8 @@ subroutine da_read_obs_bufrairs(obstype,iv,infile)
   real(kind=8)     :: tocc(1,1)
   integer          ::num_bufr(7),numbufr,ibufr
   character(20)    ::filename
-  
+  integer(i_kind), allocatable :: ptotal(:)
+
 ! Set standard parameters
   real(r_kind)     :: POinT001 =   0.001_r_kind
   real(r_kind)     :: POinT01  =   0.01_r_kind
@@ -159,7 +161,8 @@ subroutine da_read_obs_bufrairs(obstype,iv,infile)
   platform_id  = 9   ! eos series
   satellite_id = 2   ! eos-2
   nreal  = maxinfo
-  nobs(:)  = 0
+  allocate(ptotal(0:num_fgat_time))
+  ptotal(0:num_fgat_time) = 0
   airs=      obstype == 'airs     '
   eos_amsua= obstype == 'eos_amsua'
   hsb=       obstype == 'hsb      '
@@ -203,6 +206,11 @@ subroutine da_read_obs_bufrairs(obstype,iv,infile)
           .or. satellite_id /= rtminit_satid(inst) &
           .or. sensor_id /= rtminit_sensor(inst)  ) return
 
+  num_eos_file    = 0  ! number of obs in file
+  num_eos_global  = 0  ! number of obs within whole domain
+  num_eos_local   = 0  ! number of obs within tile
+  num_eos_kept    = 0  ! number of obs kept for innovation computation
+  num_eos_thinned = 0  ! number of obs rejected by thinning
   size = 0     !
   iobs = 0     ! for thinning, argument is inout
 
@@ -292,9 +300,9 @@ bufrfile:  do ibufr=1,numbufr
 
   write(unit=date,fmt='( i10)') idate
   read(unit=date,fmt='(i4,3i2)') iy,im,idd,ihh
-  write(unit=stdout,fmt=*) &
-      'Bufr file date is ',iy,im,idd,ihh,infile
-  
+  write(unit=stdout,fmt='(a,4i4,2x,a)') &
+      'Bufr file date is ',iy,im,idd,ihh,trim(infile)
+
 !   3.0 Loop over observations
 !----------------------------
      if ( ibufr == 1 ) then
@@ -315,7 +323,9 @@ bufrfile:  do ibufr=1,numbufr
         if( iret /= 0 ) exit loop_obspoints     ! end of file
         cycle loop_obspoints
      end if
-  
+
+     num_eos_file = num_eos_file + 1
+
 !   3.2 Read AQUASPOT (SPITSEQN)
 !------------------------
      call ufbseq(lnbufr,aquaspot_list_array,N_AQUASPOT_LIST,1,iret,'SPITSEQN')
@@ -395,7 +405,8 @@ bufrfile:  do ibufr=1,numbufr
         info%lat  = airsspot%clath
         info%lon  = airsspot%clonh 
         call da_llxy (info, loc, outside, outside_all )
-        if (outside) cycle loop_obspoints
+
+        if ( outside_all ) cycle loop_obspoints
 	
 !  4.1  Check obs time
 !-------------------------------------
@@ -437,29 +448,19 @@ bufrfile:  do ibufr=1,numbufr
                 obs_time  < time_slots(ifgat) ) exit
        end do
 
+       num_eos_global = num_eos_global + 1
+       ptotal(ifgat) = ptotal(ifgat) + 1
+
+       if (outside) cycle loop_obspoints
+
+       num_eos_local = num_eos_local + 1
+
        write(unit=info%date_char, &
          fmt='(i4.4,a,i2.2,a,i2.2,a,i2.2,a,i2.2,a,i2.2)')  &
          idate5(1), '-', idate5(2), '-', idate5(3), '_', idate5(4), &
          ':', idate5(5), ':', idate5(6)
 
        info%elv = 0.0  !aquaspot%selv
-
-!  4.1 Make Thinning
-!  Map obs to thinning grid
-!-------------------------------------------------------------------
-       if (thinning) then
-          dlat_earth = info%lat
-          dlon_earth = info%lon
-          if (dlon_earth<zero) dlon_earth = dlon_earth+r360
-          if (dlon_earth>=r360) dlon_earth = dlon_earth-r360
-          dlat_earth = dlat_earth*deg2rad
-          dlon_earth = dlon_earth*deg2rad           
-          crit = 1.0 ! 0.01_r_kind+terrain + timedif !+ 10.0_r_kind*float(iskip)
-	  if (airs_warmest_fov) &
-	     crit = 1E10 * exp(-(airschan(129)%tmbrst-220.0)/2)   ! warmest bt for window channel (10.36 micron)
-          call map2grids(inst,ifgat,dlat_earth,dlon_earth,crit,iobs,itx,1,itt,iout,luse)
-          if (.not. luse) cycle loop_obspoints
-       end if
 
 !  4.2  Check observational info
 !-------------------------------------------------------
@@ -486,6 +487,26 @@ bufrfile:  do ibufr=1,numbufr
 !     airdata(9) = ZERO                          ! surface height
 !     airdata(10)= POinT001                      ! land sea mask
 
+!  4.1 Make Thinning
+!  Map obs to thinning grid
+!-------------------------------------------------------------------
+       if (thinning) then
+          dlat_earth = info%lat
+          dlon_earth = info%lon
+          if (dlon_earth<zero) dlon_earth = dlon_earth+r360
+          if (dlon_earth>=r360) dlon_earth = dlon_earth-r360
+          dlat_earth = dlat_earth*deg2rad
+          dlon_earth = dlon_earth*deg2rad
+          crit = 1.0 ! 0.01_r_kind+terrain + timedif !+ 10.0_r_kind*float(iskip)
+          if (airs_warmest_fov) &
+             crit = 1E10 * exp(-(airschan(129)%tmbrst-220.0)/2)   ! warmest bt for window channel (10.36 micron)
+          call map2grids(inst,ifgat,dlat_earth,dlon_earth,crit,iobs,itx,1,itt,iout,luse)
+          if (.not. luse) then
+             num_eos_thinned = num_eos_thinned + 1
+             cycle loop_obspoints
+          end if
+       end if
+
 
 !   4.3 Retrieve Tb
 !-----------------------
@@ -505,10 +526,11 @@ bufrfile:  do ibufr=1,numbufr
           'Error in reading ', channame, ' bufr data:', &
           ' all tb data is missing'
         call da_warning(__FILE__,__LINE__,message(1:1))
+        num_eos_local = num_eos_local - 1
         cycle loop_obspoints
      end if
 
-     nobs(ifgat) = nobs(ifgat) + 1
+     num_eos_kept = num_eos_kept + 1
 
 !  4.0   assign information to sequential radiance structure
 !--------------------------------------------------------------------------
@@ -543,7 +565,7 @@ bufrfile:  do ibufr=1,numbufr
 
 end do bufrfile
 
-   if (thinning) then
+   if (thinning .and. num_eos_global > 0) then
 
 #ifdef DM_PARALLEL
          
@@ -616,6 +638,8 @@ end do bufrfile
                endif
                deallocate ( current % tb_inv )
                deallocate ( current )
+               num_eos_thinned = num_eos_thinned + 1
+               num_eos_kept = num_eos_kept - 1
                size = size - 1
                continue
             endif
@@ -641,14 +665,17 @@ end do bufrfile
    iv%total_rad_channel = iv%total_rad_channel + size*nchan
 
    iv%info(radiance)%nlocal = iv%info(radiance)%nlocal + size
-   iv%info(radiance)%ntotal = iv%info(radiance)%ntotal + size
+   iv%info(radiance)%ntotal = iv%info(radiance)%ntotal + num_eos_global
    iv%instid(inst)%info%nlocal = size
    
    do ifgat = 1, num_fgat_time
-      nobs(ifgat) = nobs(ifgat) + nobs(ifgat-1) 
-      iv%info(radiance)%ptotal(ifgat) = iv%info(radiance)%ptotal(ifgat) + nobs(ifgat) 
+      ptotal(ifgat) = ptotal(ifgat) + ptotal(ifgat-1)
+      iv%info(radiance)%ptotal(ifgat) = iv%info(radiance)%ptotal(ifgat) + ptotal(ifgat)
    end do
    
+   write(unit=stdout,fmt='(a)') '   num_eos_file num_eos_global  num_eos_local   num_eos_kept num_eos_thinned'
+   write(unit=stdout,fmt='(5(5x,i10))') num_eos_file,num_eos_global, num_eos_local,num_eos_kept,num_eos_thinned
+
 !  5.0 allocate innovation radiance structure
 !----------------------------------------------------------------
 !  do i = 1, iv%num_inst
@@ -683,6 +710,9 @@ end do bufrfile
    deallocate ( current )
 
  end do
+
+ deallocate (p)
+ deallocate (ptotal)
 
 1000 continue
    call closbf(lnbufr)

var/da/da_radiance/da_read_obs_bufrseviri.inc
@@ -51,7 +51,6 @@ subroutine da_read_obs_bufrseviri (obstype,iv,infile)
 
   logical            :: clrsky,allsky,allchnmiss
   real               :: rclrsky
-  integer            :: ndata,nodata !nread,ndata,nodata
   integer :: kidsat 
   integer(i_kind)    :: idate5(6)
   integer (i_kind), allocatable :: ptotal(:), nread(:) 
@@ -121,10 +120,6 @@ subroutine da_read_obs_bufrseviri (obstype,iv,infile)
   real(r_kind),parameter:: tbmax  = 550._r_kind
   real(r_kind),parameter:: earth_radius = 6371000._r_kind
 
-  nread=0
-  ndata=0
-  nodata=0
-
   ilath=8                      ! the position of latitude in the header
   ilonh=9                      ! the position of longitude in the header
   ilzah=10                     ! satellite zenith angle
@@ -137,7 +132,6 @@ subroutine da_read_obs_bufrseviri (obstype,iv,infile)
   !  0.0  Initialize variables
   !-----------------------------------
   platform_id  = 3   ! metereosat
-  satellite_id = 9   ! 8-10 
   sensor_id = 21 !seviri
   disterrmax=zero
   ntest=0
@@ -265,8 +259,8 @@ subroutine da_read_obs_bufrseviri (obstype,iv,infile)
    sensorindex=1  
    write(unit=date,fmt='( i10)') idate
    read(unit=date,fmt='(i4,3i2)') iy,im,idd,ihh
-   write(unit=stdout,fmt=*) &
-      'Bufr file date is ',iy,im,idd,ihh,infile
+   write(unit=stdout,fmt='(a,4i4,2x,a)') &
+      'Bufr file date is ',iy,im,idd,ihh,trim(infile)
 
    ! 2.0 Loop to read bufr file and assign information to a sequential structure
    !-------------------------------------------------------------------------
@@ -296,6 +290,13 @@ subroutine da_read_obs_bufrseviri (obstype,iv,infile)
         if ( ( kidsat > 57) .or. ( kidsat < 55) ) then 
            write(unit=message(1),fmt='(A,I6)') 'Unknown platform: ', kidsat
            call da_warning(__FILE__,__LINE__,message(1:1))
+        end if
+        if ( kidsat == 55 ) then
+            satellite_id = 8
+        else if ( kidsat == 56 ) then
+            satellite_id = 9
+        else if ( kidsat == 57 ) then
+            satellite_id = 10
         end if
 
         if (clrsky) then     ! asr bufr has no sza
@@ -564,8 +565,8 @@ deallocate(data_all) ! Deallocate data arrays
       call da_warning(__FILE__,__LINE__,message(1:1))
    endif
 
-   write(unit=stdout,fmt='(a)') 'num_seviri_file, num_seviri_global, num_seviri_local, num_seviri_used, num_seviri_thinned'
-   write(stdout,*) num_seviri_file, num_seviri_global, num_seviri_local, num_seviri_used, num_seviri_thinned
+   write(unit=stdout,fmt='(a)') '   num_seviri_file num_seviri_global  num_seviri_local   num_seviri_used num_seviri_thinned'
+   write(stdout,'(5(8x,i10))') num_seviri_file, num_seviri_global, num_seviri_local, num_seviri_used, num_seviri_thinned
 
   
 

var/da/da_setup_structures/da_setup_be_regional.inc
@@ -755,10 +755,11 @@ subroutine da_setup_be_regional(xb, be, grid)
     read (be_unit, IOSTAT=k) variable
          if (print_detail_be)&
           write(unit=stdout,fmt='(2a)')' Reading lengthscale for: ',trim(adjustl(variable))
-         if( k/=0 ) &
+         if( k/=0 ) then
             write(unit=message(1),fmt='(A,I0,A,I0,A,A)') 'Error ',k,' reading namv(', &
                 i,')=',namv(i)
             call da_warning(__FILE__,__LINE__,message(1:1))
+         end if
    select case( trim(adjustl(variable)) )
    case ('psi')
      read(be_unit) rfls1_be

var/da/da_setup_structures/da_setup_obs_structures.inc
@@ -254,15 +254,16 @@ subroutine da_setup_obs_structures( grid, ob, iv, j_cost)
             ob_name = obs_names(j)
          endif
          if(iv%info(j)%ptotal(i) - iv%info(j)%ptotal(i-1) > 0) then          
-         if( thin_conv) then
-         write(unit=stdout, fmt='(2x,2(a,i6),a,i5,a)') &
-            ob_name, iv%info(j)%ptotal(i) - iv%info(j)%ptotal(i-1), ' global, ', &
-                     iv%info(j)%thin_ptotal(i) - iv%info(j)%thin_ptotal(i-1), ' global (post-thinning), ', &
-            iv%info(j)%thin_plocal(i) - iv%info(j)%thin_plocal(i-1), ' local (post-thinning)'
+         if ( thin_conv .and. ob_format == ob_format_ascii ) then
+            if ( j == radiance ) cycle !temporary fix to not print incorrect counts for radiance
+            write(unit=stdout, fmt='(2x,2(a,i10),a,i8,a)') &
+               ob_name, iv%info(j)%ptotal(i) - iv%info(j)%ptotal(i-1), ' global, ', &
+               iv%info(j)%thin_ptotal(i) - iv%info(j)%thin_ptotal(i-1), ' global (post-thinning), ', &
+               iv%info(j)%thin_plocal(i) - iv%info(j)%thin_plocal(i-1), ' local (post-thinning)'
          else
-         write(unit=stdout, fmt='(6x,a,i6,a,i5,a)') &
-            ob_name, iv%info(j)%ptotal(i) - iv%info(j)%ptotal(i-1), ' global,', &
-            iv%info(j)%plocal(i) - iv%info(j)%plocal(i-1), ' local'
+            write(unit=stdout, fmt='(6x,a,i10,a,i8,a)') &
+               ob_name, iv%info(j)%ptotal(i) - iv%info(j)%ptotal(i-1), ' global,', &
+               iv%info(j)%plocal(i) - iv%info(j)%plocal(i-1), ' local'
          end if ! thin_conv
          end if
       end do

var/obsproc/src/module_err_afwa.F90
@@ -1833,10 +1833,10 @@ SUBROUTINE READ_OBSERR_HEIGHT (iunit, filein, keyword, endword, fmt_err, err)
 
       WRITE (UNIT = iunit, FMT = '(20X,A)') TRIM (keyword)
 
-      WRITE (UNIT = iunit, FMT = '(1X,16A)') ' level',&
+      WRITE (UNIT = iunit, FMT = '(18A)')   ' level ',&
     ' synop ','  ship ','  buoy ',' metar ',' pilot ',' profl ',&
-    ' sound ',' satem ',' satob ',' airep ',' tamdar ',&
-    ' ssmt1 ',' ssmt2 ','  tovs ','  ssmi ',' airsret',&
+    ' sound ',' satem ',' satob ',' airep ','tamdar ',&
+    ' ssmt1 ',' ssmt2 ','  tovs ','  ssmi ',' airsr ',&
     ' other '
 
       DO i = 15, 1, -1
@@ -2161,13 +2161,13 @@ SUBROUTINE READ_OBSERR_WIND (iunit, filein, keyword, endword, fmt_err, err)
 
       WRITE (UNIT = iunit, FMT = '(20X,A)') TRIM (keyword)
 
-      WRITE (UNIT = iunit, FMT = '(1X,16A)') ' level',&
+      WRITE (UNIT = iunit, FMT = '(18A)')   ' level ',&
     ' synop ','  ship ','  buoy ',' metar ',' pilot ',' profl ',&
-    ' sound ',' satem ',' satob ',' airep ',' tamdar ',&
-    ' ssmt1 ',' ssmt2 ','  ssmi ','  tovs ',' other '
+    ' sound ',' satem ',' satob ',' airep ','tamdar ',&
+    ' ssmt1 ',' ssmt2 ','  ssmi ','  tovs ',' airsr ',' other '
 
       DO i = 26, 1, -1
-         WRITE (UNIT = iunit, FMT = '(F6.0,16(2X,F5.1))') &
+         WRITE (UNIT = iunit, FMT = '(F6.0,17(2X,F5.1))') &
                 err (i) % level / 100., &
                 err (i) % synop % wind, &
                 err (i) % ships % wind, &
@@ -2184,6 +2184,7 @@ SUBROUTINE READ_OBSERR_WIND (iunit, filein, keyword, endword, fmt_err, err)
                 err (i) % ssmt2 % wind, &
                 err (i) % ssmi  % wind, &
                 err (i) % tovs  % wind, &
+                -99.9, &
                 err (i) % other % wind
 
 
@@ -2484,13 +2485,13 @@ SUBROUTINE READ_OBSERR_DIR (iunit, filein, keyword, endword, fmt_err, err)
 
       WRITE (UNIT = iunit, FMT = '(20X,A)') TRIM (keyword)
 
-      WRITE (UNIT = iunit, FMT = '(1X,16A)') ' level',&
+      WRITE (UNIT = iunit, FMT = '(18A)')  ' level ',&
     ' synop ','  ship ','  buoy ',' metar ',' pilot ',' profl ',&
-    ' sound ',' satem ',' satob ',' airep ',' tamdar ',&
-    ' ssmt1 ',' ssmt2 ','  ssmi ','  tovs ',' other '
+    ' sound ',' satem ',' satob ',' airep ','tamdar ',&
+    ' ssmt1 ',' ssmt2 ','  ssmi ','  tovs ',' airsr ',' other '
 
       DO i = 26, 1, -1
-         WRITE (UNIT = iunit, FMT = '(F6.0,16(2X,F5.1))') &
+         WRITE (UNIT = iunit, FMT = '(F6.0,17(2X,F5.1))') &
                 err (i) % level / 100., &
                 err (i) % synop % dir , &
                 err (i) % ships % dir , &
@@ -2507,6 +2508,7 @@ SUBROUTINE READ_OBSERR_DIR (iunit, filein, keyword, endword, fmt_err, err)
                 err (i) % ssmt2 % dir , &
                 err (i) % ssmi  % dir , &
                 err (i) % tovs  % dir , &
+                -99.9, &
                 err (i) % other % dir 
 
 
@@ -2786,10 +2788,10 @@ SUBROUTINE READ_OBSERR_TEMP (iunit, filein, keyword, endword, fmt_err, err)
 
       WRITE (UNIT = iunit, FMT = '(20X,A)') TRIM (keyword)
 
-      WRITE (UNIT = iunit, FMT = '(1X,16A)') ' level',&
+      WRITE (UNIT = iunit, FMT = '(18A)')   ' level ',&
     ' synop ','  ship ','  buoy ',' metar ',' pilot ',' profl ',&
-    ' sound ',' satem ',' satob ',' airep ',' tamdar ',&
-    ' ssmt1 ',' ssmt2 ','  tovs ','  ssmi ',' airsret', &
+    ' sound ',' satem ',' satob ',' airep ','tamdar ',&
+    ' ssmt1 ',' ssmt2 ','  tovs ','  ssmi ',' airsr ', &
     ' other '
 
 
@@ -3074,10 +3076,10 @@ SUBROUTINE READ_OBSERR_RH (iunit, filein, keyword, endword, fmt_err, err)
 
       WRITE (UNIT = iunit, FMT = '(20X,A)') TRIM (keyword)
 
-      WRITE (UNIT = iunit, FMT = '(1X,16A)') ' level',&
+      WRITE (UNIT = iunit, FMT = '(18A)')   ' level ',&
     ' synop ','  ship ','  buoy ',' metar ',' pilot ',' profl ',&
-    ' sound ',' satem ',' satob ',' airep ',' tamdar ',&
-    ' ssmt1 ',' ssmt2 ','  tovs ','  ssmi ',' airsret',&
+    ' sound ',' satem ',' satob ',' airep ','tamdar ',&
+    ' ssmt1 ',' ssmt2 ','  tovs ','  ssmi ',' airsr ',&
     ' other '
 
       DO i = 15, 1, -1
@@ -3362,10 +3364,10 @@ SUBROUTINE READ_OBSERR_PRES (iunit, filein, keyword, endword, fmt_err, err)
 
       WRITE (UNIT = iunit, FMT = '(20X,A)') TRIM (keyword)
 
-      WRITE (UNIT = iunit, FMT = '(1X,16A)') ' level',&
+      WRITE (UNIT = iunit, FMT = '(18A)')   ' level ',&
     ' synop ','  ship ','  buoy ',' metar ',' pilot ',' profl ',&
-    ' sound ',' satem ',' satob ',' airep ',' tamdar ',&
-    ' ssmt1 ',' ssmt2 ','  tovs ','  ssmi ', 'airsret', &
+    ' sound ',' satem ',' satob ',' airep ','tamdar ',&
+    ' ssmt1 ',' ssmt2 ','  tovs ','  ssmi ',' airsr ', &
     ' other '
 
       DO i = 15, 1, -1

var/run/VARBC.in
@@ -1,5 +1,5 @@
   VARBC version 1.0 - Number of instruments: 
- 29
+ 30
  ------------------------------------------------
  Platform_id  Sat_id  Sensor_id  Nchanl  Npredmax
  ------------------------------------------------
@@ -1429,6 +1429,23 @@
  Platform_id  Sat_id  Sensor_id  Nchanl  Npredmax
  ------------------------------------------------
  3 9 21 8 8
+  -----> Bias predictor statistics:  Mean & Std & Nbgerr
+       0.0       0.0       0.0       0.0       0.0       0.0       0.0       0.0       0.0
+       0.0       0.0       0.0       0.0       0.0       0.0       0.0       0.0       0.0
+     10000     10000     10000     10000     10000     10000     10000     10000     10000
+  -----> Chanl_id Chanl_nb  Pred_use(-1/0/1)  Param
+   1     1  0  0  0  0  0  0  0  0   4.457  -0.211  -0.054   0.213  -0.101   1.770  -0.283  -0.794
+   2     2  0  0  0  0  0  0  0  0   4.457  -0.211  -0.054   0.213  -0.101   1.770  -0.283  -0.794
+   3     3  0  0  0  0  0  0  0  0   4.457  -0.211  -0.054   0.213  -0.101   1.770  -0.283  -0.794
+   4     4  0  0  0  0  0  0  0  0   4.457  -0.211  -0.054   0.213  -0.101   1.770  -0.283  -0.794
+   5     5  0  0  0  0  0  0  0  0   4.457  -0.211  -0.054   0.213  -0.101   1.770  -0.283  -0.794
+   6     6  0  0  0  0  0  0  0  0   1.195   0.009  -0.048   0.082  -0.055  -1.055   0.894  -0.481
+   7     7  0  0  0  0  0  0  0  0   1.653   0.037  -0.072   0.066  -0.097  -1.610   0.345   0.302
+   8     8  0  0  0  0  0  0  0  0   0.582   0.253  -0.041  -0.042  -0.031  -0.803   0.227   0.076
+ ------------------------------------------------
+ Platform_id  Sat_id  Sensor_id  Nchanl  Npredmax
+ ------------------------------------------------
+ 3 10 21 8 8
   -----> Bias predictor statistics:  Mean & Std & Nbgerr
        0.0       0.0       0.0       0.0       0.0       0.0       0.0       0.0       0.0
        0.0       0.0       0.0       0.0       0.0       0.0       0.0       0.0       0.0

var/run/radiance_info/meteosat-10-seviri.info
@@ -1,0 +1,9 @@
+sensor channel IR/MW use idum  varch  polarisation(0:vertical;1:horizontal)
+   207    1    1    1    0  1.8000000000E+00  2.0000000000E+00  msg-3-seviri
+   207    2    1    1    0  2.5000000000E+00  2.0000000000E+00
+   207    3    1    1    0  2.2500000000E+00  2.0000000000E+00
+   207    4    1    1    0  1.2500000000E+00  2.0000000000E+00
+   207    5    1    1    0  1.2500000000E+00  2.0000000000E+00
+   207    6    1    1    0  1.2500000000E+00  2.0000000000E+00
+   207    7    1    1    0  1.4500000000E+00  2.0000000000E+00
+   207    8    1    1    0  1.2500000000E+00  3.0000000000E+00
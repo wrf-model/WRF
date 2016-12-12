wrf-model/WRF

commit af26bfbaad89a9648d602f50667323b311b31c86

Hybrid vertical coordinate mods for WRFDA 3DVAR and da_update_bc. (#56)
TYPE: new feature

KEYWORDS: WRFDA, hybrid vertical coordinate, hybrid_opt, 3DVAR, da_update_bc

SOURCE: internal

DESCRIPTION OF CHANGES:

The changes to MU and MUB are as described in Registry/registry.hyb_coord (from Dave).
For now, the code (da_solve.inc) checks the namelist option (config_flags%hybrid_opt).
Eventually, the global attribute HYBRID_OPT should be available and should be used for checking.

Hybrid vertical coordinate for WRFDA 4DVAR is not implemented yet.
It is scheduled for the next release after V3.9.

LIST OF MODIFIED FILES:
M       Registry/Registry.wrfvar
A       Registry/registry.hyb_coord
M       var/build/depend.txt
M       var/da/da_control/da_control.f90
M       var/da/da_main/da_solve.inc
M       var/da/da_transfer_model/da_transfer_model.f90
M       var/da/da_transfer_model/da_transfer_wrftoxb.inc
M       var/da/da_transfer_model/da_transfer_xatowrf.inc
M       var/da/da_update_bc/da_couple.inc
M       var/da/da_update_bc/da_couple_uv.inc
M       var/da/da_update_bc/da_get_gl_att_int_cdf.inc
M       var/da/da_update_bc/da_get_gl_att_real_cdf.inc
A       var/da/da_update_bc/da_get_var_1d_real_cdf.inc
M       var/da/da_update_bc/da_netcdf_interface.f90
M       var/da/da_update_bc/da_update_bc.f90
M       var/da/da_vtox_transforms/da_transform_xtoxa.inc
M       var/da/da_vtox_transforms/da_transform_xtoxa_adj.inc
M       var/da/da_vtox_transforms/da_vtox_transforms.f90

TESTS CONDUCTED:
1. WRFDA regtest results remain unchanged since they are all in the original coordinate.
2. DA 3DVAR cycling runs to test the hybrid coordinate will be done later.

var/da/da_transfer_model/da_transfer_model.f90
@@ -42,6 +42,7 @@ module da_transfer_model
       its,ite,jts,jte,kts,kte, ips,ipe,jps,jpe,kps,kpe, qlimit, &
       update_sfcdiags, use_wrf_sfcinfo
    use da_control, only: base_pres_strat, base_lapse_strat
+   use da_control, only: c1h, c2h, c3f, c3h, c4f, c4h
    use da_define_structures, only : xbx_type, be_type
    use da_par_util, only : da_patch_to_global
    use da_physics, only : da_check_rh_simple,da_roughness_from_lanu, &

Registry/Registry.wrfvar
@@ -3,6 +3,7 @@ include registry.dimspec
 include Registry.EM_COMMON.var
 include registry.io_boilerplate
 include registry.var
+include registry.hyb_coord
 
 state   real    -              ijkft   a_scalar    1         -     -    -                        
 state   real    -              ijkft   g_scalar    1         -     -    -                       

Registry/registry.hyb_coord
@@ -1,0 +1,56 @@
+#	Dry pressure, Pd
+#	Dry surface pressure = Pds
+#	Model top pressure = Pt
+#	Mass in column, Pc = Pds - Pt
+
+#	Total dry pressure
+#	Pd = BF ( Pds - Pt ) + ( eta - BF ) ( P0 - Pt ) + Pt
+
+#	Hybrid coordinate: mu is still d(Pd)/d(eta)
+#	new MUT = d Pd / d eta = d BF / d eta * ( Pcb + Pc ) + ( 1 - d BF / d eta ) * ( P0 - Pt )
+
+#	Define two columnar constants, function only of eta and other constants, specifically for the "mu" replacements.
+#	C1 = d BF / d eta
+#	C2 = ( 1 - d BF / d eta ) * ( P0 - Pt )
+
+#	Total field
+#	new              MUT(i,k,j) = C1(k) * ( Pcb(i,j) + Pc(i,j) ) + C2(k)
+
+#	Base-state, background field
+#	new              MUB(i,k,j) = C1(k) *   Pcb(i,j)             + C2(k)
+
+#	Perturbation field
+#	new              MU(i,k,j)  = C1(k) *              Pc(i,j)
+
+#	Define two columnar constants, function only of eta and other constants, specifically for the "pressure" replacements.
+#	C3 = BF
+#	C4 = ( eta - BF ) * ( P0 - Pt )
+
+#	new dry pressure Pd(i,k,j)  = C3(k) * ( Pds(i,j) - Pt      ) + C4(k) + Pt
+ 
+
+#<Table> <Type>   <Sym>         <Dims>   <Use>           <NumTLev> <Stagger>        <IO>            <DNAME>                        <DESCRIP>                            <UNITS>
+
+state    real      bf              k     misc                1         Z     i02rh0{22}{23}{24}      "BF"        "full levels, bf=0 => isobaric; bf=znw => sigma"     "Dimensionless"
+state    real      c1h             k     misc                1         -     i02rh0{22}{23}{24}      "C1H"       "half levels, c1h = d bf / d eta, using znw"         "Dimensionless"
+state    real      c2h             k     misc                1         -     i02rh0{22}{23}{24}      "C2H"       "half levels, c2h = (1-c1h)*(p0-pt)"                 "Pa"
+
+state    real      bh              k     misc                1         -     i02rh0{22}{23}{24}      "BH"        "half levels, bh=0 => isobaric; bh=znu => sigma"     "Dimensionless"
+state    real      c1f             k     misc                1         Z     i02rh0{22}{23}{24}      "C1F"       "full levels, c1f = d bf / d eta, using znu"         "Dimensionless"
+state    real      c2f             k     misc                1         Z     i02rh0{22}{23}{24}      "C2F"       "full levels, c2f = (1-c1f)*(p0-pt)"                 "Pa"
+
+state    real      c3h             k     misc                1         -     i02rh0{22}{23}{24}      "C3H"       "half levels, c3h = bh"                             "Dimensionless"
+state    real      c4h             k     misc                1         -     i02rh0{22}{23}{24}      "C4H"       "half levels, c4h = (eta-bh)*(p0-pt)+pt, using znu" "Pa"
+
+state    real      c3f             k     misc                1         Z     i02rh0{22}{23}{24}      "C3F"       "full levels, c3f = bf"                             "Dimensionless"
+state    real      c4f             k     misc                1         Z     i02rh0{22}{23}{24}      "C4F"       "full levels, c4f = (eta-bf)*(p0-pt)+pt, using znw" "Pa"
+
+state    real      pcb            ij    dyn_em               1         -     irhdus                  "PCB"       "base state dry air mass in column"                 "Pa"
+state    real      pc             ijb   dyn_em               2         -     irhusdf=(bdy_interp:dt) "PC"        "perturbation dry air mass in column"               "Pa"
+
+
+
+
+#<Table> <Type>   <Sym>         <Nml>                    <NumEntry> <Default>                                                     <DESCRIP>                             <UNITS>
+rconfig  integer  hybrid_opt    namelist,dynamics            1          0     i0                  "HYBRID_OPT"   "0=Original WRF coordinate, 1=Terrain Following using hybrid formulation, 2=Klemp cubic form with etac"         "Flag"
+rconfig  real     etac          namelist,dynamics            1         -1.    i0                  "ETAC"         "znw(k) < etac, eta surfaces are isobaric"          "Dimensionless"

var/da/da_control/da_control.f90
@@ -17,6 +17,12 @@ module da_control
    logical :: use_rad
 
    !---------------------------------------------------------------------------
+   ! [0.0] WRF hybrid coordinate variables
+   !---------------------------------------------------------------------------
+   real, allocatable :: c1f(:), c2f(:), c3f(:), c4f(:)
+   real, allocatable :: c1h(:), c2h(:), c3h(:), c4h(:)
+
+   !---------------------------------------------------------------------------
    ! [1.0] Physical parameter constants (all NIST standard values):
    !---------------------------------------------------------------------------
 

var/da/da_main/da_solve.inc
@@ -297,6 +297,34 @@
    ! [3.0] Set up first guess field (grid%xb):
    !---------------------------------------------------------------------------
 
+   ! for WRF hybrid coordinate
+   allocate ( c1f(kms:kme) )
+   allocate ( c2f(kms:kme) )
+   allocate ( c3f(kms:kme) )
+   allocate ( c4f(kms:kme) )
+   allocate ( c1h(kms:kme) )
+   allocate ( c2h(kms:kme) )
+   allocate ( c3h(kms:kme) )
+   allocate ( c4h(kms:kme) )
+   c1f = grid%c1f
+   c2f = grid%c2f
+   c3f = grid%c3f
+   c4f = grid%c4f
+   c1h = grid%c1h
+   c2h = grid%c2h
+   c3h = grid%c3h
+   c4h = grid%c4h
+   if ( config_flags%hybrid_opt == 0 ) then
+      c3f = grid%znw
+      c3h = grid%znu
+      c4f = 0.0
+      c4h = 0.0
+      c1f = 1.0
+      c1h = 1.0
+      c2f = 0.0
+      c2h = 0.0
+   end if
+
    call da_setup_firstguess(xbx, grid, config_flags, .false.)
 
    if ( anal_type_hybrid_dual_res ) then
@@ -964,7 +992,14 @@
 
    if ( anal_type_hybrid_dual_res ) deallocate(ob_locs)
 
-
+   deallocate ( c1f )
+   deallocate ( c2f )
+   deallocate ( c3f )
+   deallocate ( c4f )
+   deallocate ( c1h )
+   deallocate ( c2h )
+   deallocate ( c3h )
+   deallocate ( c4h )
 
 #ifdef DM_PARALLEL
    call mpi_barrier (comm,ierr)

var/da/da_transfer_model/da_transfer_wrftoxb.inc
@@ -366,7 +366,7 @@ subroutine da_transfer_wrftoxb(xbx, grid, config_flags)
           do j=grid%j_start(ij), grid%j_end(ij)
             do i=its,ite
                ! The base specific volume (from real.init.code)
-               ppb  = grid%znu(k) * grid%mub(i,j) + ptop
+               ppb  = c3h(k) * grid%mub(i,j) + c4h(k) + ptop
                grid%pb(i,j,k) = ppb
                temp = MAX ( iso_temp, base_temp + base_lapse*log(ppb/base_pres) )
                if ( grid%pb(i,j,k) < base_pres_strat ) then
@@ -378,8 +378,8 @@ subroutine da_transfer_wrftoxb(xbx, grid, config_flags)
                albn = (gas_constant/base_pres) * ttb * (ppb/base_pres)**cvpm
 
                qvf1 = 1.0 + grid%moist(i,j,k,P_QV) / rd_over_rv
-               aln  = -1.0 / (grid%mub(i,j)+grid%mu_2(i,j)) * &
-                      (albn*grid%mu_2(i,j) + grid%rdnw(k) * &
+               aln  = -1.0 / (c1h(k)*(grid%mub(i,j)+grid%mu_2(i,j))+c2h(k)) * &
+                      (albn*(c1h(k)*grid%mu_2(i,j)) + grid%rdnw(k) * &
                       (grid%ph_2(i,j,k+1) - grid%ph_2(i,j,k)))
                ! total pressure:
                grid%xb%p(i,j,k) = base_pres * &
@@ -397,7 +397,7 @@ subroutine da_transfer_wrftoxb(xbx, grid, config_flags)
           do i=its,ite
             p_surf = base_pres * EXP ( -base_temp/base_lapse + ( (base_temp/base_lapse)**2 - 2.*gravity*grid%ht(i,j)/base_lapse/gas_constant ) **0.5 )
             do k = kts, kte
-               grid%pb(i,j,k) = grid%znu(k)*(p_surf - grid%p_top) + grid%p_top
+               grid%pb(i,j,k) = c3h(k)*(p_surf - grid%p_top) + c4h(k) + grid%p_top
                temp = MAX ( iso_temp, base_temp + base_lapse*log(grid%pb(i,j,k)/base_pres) )
                if ( grid%pb(i,j,k) < base_pres_strat ) then
                   temp = iso_temp + base_lapse_strat*log(grid%pb(i,j,k)/base_pres_strat)
@@ -409,13 +409,13 @@ subroutine da_transfer_wrftoxb(xbx, grid, config_flags)
             grid%phb(i,j,1) = grid%ht(i,j) * gravity
             if (grid%hypsometric_opt == 1) then
                do k = kts,kte
-                  grid%phb(i,j,k+1) = grid%phb(i,j,k) - grid%dnw(k)*grid%mub(i,j)*grid%alb(i,j,k)
+                  grid%phb(i,j,k+1) = grid%phb(i,j,k) - grid%dnw(k)*(c1h(k)*grid%mub(i,j)+c2h(k))*grid%alb(i,j,k)
                end do
             else if (grid%hypsometric_opt == 2) then
                do k = kts,kte
-                  pfu = grid%mub(i,j)*grid%znw(k+1) + grid%p_top
-                  pfd = grid%mub(i,j)*grid%znw(k  ) + grid%p_top
-                  phm = grid%mub(i,j)*grid%znu(k  ) + grid%p_top
+                  pfu = grid%mub(i,j)*c3f(k+1) + c4f(k+1) + grid%p_top
+                  pfd = grid%mub(i,j)*c3f(k  ) + c4f(k  ) + grid%p_top
+                  phm = grid%mub(i,j)*c3h(k  ) + c4h(k  ) + grid%p_top
                   grid%phb(i,j,k+1) = grid%phb(i,j,k) + grid%alb(i,j,k)*phm*LOG(pfd/pfu)
                end do
             end if
@@ -426,7 +426,8 @@ subroutine da_transfer_wrftoxb(xbx, grid, config_flags)
            do k=kts,kte
              do j=grid%j_start(ij), grid%j_end(ij)
                do i=its,ite
-                grid%al(i,j,k)=-1./(grid%mub(i,j)+grid%mu_2(i,j))*(grid%alb(i,j,k)*grid%mu_2(i,j)  &
+                grid%al(i,j,k)=-1./(c1h(k)*(grid%mub(i,j)+grid%mu_2(i,j))+c2h(k))* &
+                     (grid%alb(i,j,k)*(c1h(k)*grid%mu_2(i,j))  &
                      +grid%rdnw(k)*(grid%ph_2(i,j,k+1)-grid%ph_2(i,j,k)))
                 ! total density
                 grid%xb%rho(i,j,k)= 1.0 / (grid%alb(i,j,k)+grid%al(i,j,k))
@@ -437,9 +438,9 @@ subroutine da_transfer_wrftoxb(xbx, grid, config_flags)
            do k=kts,kte
              do j=grid%j_start(ij), grid%j_end(ij)
               do i=its,ite
-               pfu = (grid%mub(i,j)+grid%mu_2(i,j))*grid%znw(k+1)+grid%p_top
-               pfd = (grid%mub(i,j)+grid%mu_2(i,j))*grid%znw(k)  +grid%p_top
-               phm = (grid%mub(i,j)+grid%mu_2(i,j))*grid%znu(k)  +grid%p_top
+               pfu = (grid%mub(i,j)+grid%mu_2(i,j))*c3f(k+1)+c4f(k+1)+grid%p_top
+               pfd = (grid%mub(i,j)+grid%mu_2(i,j))*c3f(k)  +c4f(k)  +grid%p_top
+               phm = (grid%mub(i,j)+grid%mu_2(i,j))*c3h(k)  +c4h(k)  +grid%p_top
                grid%al(i,j,k) = (grid%ph_2(i,j,k+1)-grid%ph_2(i,j,k)+grid%phb(i,j,k+1)-grid%phb(i,j,k)) &
                                  /phm/LOG(pfd/pfu)-grid%alb(i,j,k)
                ! total density
@@ -475,8 +476,8 @@ subroutine da_transfer_wrftoxb(xbx, grid, config_flags)
         ! The full and half level dry hydrostatic pressure is easily computed (YRG, 12/20/2011):
         do i=its,ite
            do k=kts, kte+1
-              pf(i,k) = (grid%mub(i,j)+grid%mu_2(i,j)) * grid%znw(k) + ptop
-              pp(i,k) = (grid%mub(i,j)+grid%mu_2(i,j)) * grid%znu(k) + ptop
+              pf(i,k) = (grid%mub(i,j)+grid%mu_2(i,j)) * c3f(k) + c4f(k) + ptop
+              pp(i,k) = (grid%mub(i,j)+grid%mu_2(i,j)) * c3h(k) + c4h(k) + ptop
            enddo
         enddo
       endif

var/da/da_transfer_model/da_transfer_xatowrf.inc
@@ -120,8 +120,8 @@ subroutine da_transfer_xatowrf(grid, config_flags)
          qvf1   = qvf1*qvf2_b + qvf1_b*qvf2
          qvf1_b = qvf1_b*qvf2_b
          grid%xa%p(i,j,k) = (-0.5/grid%rdnw(k)) * &
-                    ((mu_cgrid(i,j)+qvf1*grid%mub(i,j)) / qvf2_b &
-                     -(grid%mu_2(i,j)+qvf1_b*grid%mub(i,j))*qvf2/(qvf2_b*qvf2_b))
+                    ((c1f(k)*mu_cgrid(i,j)+qvf1*c1f(k)*grid%mub(i,j)) / qvf2_b &
+                     -(c1f(k)*grid%mu_2(i,j)+qvf1_b*c1f(k)*grid%mub(i,j))*qvf2/(qvf2_b*qvf2_b))
 
          do k = kte-1,1,-1
             qvf1   = 0.5*(q_cgrid(i,j,k)+q_cgrid(i,j,k+1))
@@ -132,8 +132,8 @@ subroutine da_transfer_xatowrf(grid, config_flags)
             qvf1_b = qvf1_b*qvf2_b
             grid%xa%p(i,j,k) = grid%xa%p(i,j,k+1)  &
                        - (1.0/grid%rdn(k+1)) * &
-                       ((mu_cgrid(i,j)+qvf1*grid%mub(i,j)) / qvf2_b &
-                        -(grid%mu_2(i,j)+qvf1_b*grid%mub(i,j))*qvf2/(qvf2_b*qvf2_b))
+                       ((c1f(k)*mu_cgrid(i,j)+qvf1*c1f(k)*grid%mub(i,j)) / qvf2_b &
+                        -(c1f(k)*grid%mu_2(i,j)+qvf1_b*c1f(k)*grid%mub(i,j))*qvf2/(qvf2_b*qvf2_b))
          end do
 
       end do
@@ -244,13 +244,16 @@ subroutine da_transfer_xatowrf(grid, config_flags)
             ! To compute the theta increment with the full fields:
             grid%t_2(i,j,k) = t_full*(base_pres/p_full)**kappa - t0
 
-            ! The full fiteld of analysis ph:
+            ! The full field of analysis ph:
             ph_full  = ph_full  &
-                       - grid%xb%dnw(k) * (grid%xb%psac(i,j)+mu_cgrid(i,j)) / rho_dry
+                       - grid%xb%dnw(k) * &
+                       ((c1h(k)*grid%xb%psac(i,j)+c2h(k))+c1h(k)*mu_cgrid(i,j)) &
+                       / rho_dry
 
             ! background hydrostatic phi:
             ph_xb_hd  = ph_xb_hd  &
-                       - grid%xb%dnw(k) * grid%xb%psac(i,j) / grid%xb%rho(i,j,k)
+                       - grid%xb%dnw(k) * &
+                       (c1h(k)*grid%xb%psac(i,j)+c2h(k)) / grid%xb%rho(i,j,k)
 
             ! The analysis perturbation = Hydro_ph - base_ph + nonhydro_xb_ph:
             grid%ph_2(i,j,k+1) = ph_full - grid%phb(i,j,k+1) &
@@ -268,12 +271,10 @@ subroutine da_transfer_xatowrf(grid, config_flags)
    DO j=jts,jte
    DO i=its,ite
 
-      ! dry air density
       mu_full = grid%mub(i,j)+grid%mu_2(i,j)+mu_cgrid(i,j)
-      ph      = 0.0
 
       ! Compute geopotential (using dry inverse density and dry pressure)
-
+      ph      = 0.0
       ph(kts) = grid%ht(i,j) * gravity
       DO k = kts, kte
          p_full = grid%xb%p(i,j,k) + grid%xa%p(i,j,k)
@@ -290,9 +291,9 @@ subroutine da_transfer_xatowrf(grid, config_flags)
 
       ! Dry mass is purely hydrostatic: Native approach in WRF
 
-          pfu = mu_full*grid%znw(k+1) + grid%p_top
-          pfd = mu_full*grid%znw(k)   + grid%p_top
-          phm = mu_full*grid%znu(k)   + grid%p_top
+          pfu = mu_full*c3f(k+1) + c4f(k+1) + grid%p_top
+          pfd = mu_full*c3f(k)   + c4f(k)   + grid%p_top
+          phm = mu_full*c3h(k)   + c4h(k)   + grid%p_top
           ph(k+1) = ph(k) + ald(k)*phm*LOG(pfd/pfu) 
           grid%ph_2(i,j,k+1) = ph(k+1) - grid%phb(i,j,k+1)
        END DO

var/da/da_update_bc/da_couple.inc
@@ -1,21 +1,23 @@
-subroutine da_couple (mut, field, msf, ids, ide, jds, jde, kds, kde)
+subroutine da_couple (mut, field, msf, c1, c2, ids, ide, jds, jde, kds, kde)
 
    implicit none
 
    integer, intent(in)    :: ids, ide, jds, jde, kds, kde
    real,    intent(in)    :: mut(ids:ide, jds:jde)
    real,    intent(in)    :: msf(ids:ide, jds:jde)
+   real,    intent(in)    :: c1(kds:kde)
+   real,    intent(in)    :: c2(kds:kde)
    real,    intent(inout) :: field(ids:ide, jds:jde, kds:kde)
-  
+
    integer :: i, j, k
 
    ! update_bc
    ! if (trace_use) call da_trace_entry("da_couple")
-  
-   do j=jds,jde
-      do k=kds,kde
+
+   do k=kds,kde
+      do j=jds,jde
          do i=ids,ide
-            field(i,j,k)=field(i,j,k)*mut(i,j)/msf(i,j)
+            field(i,j,k)=field(i,j,k)*(c1(k)*mut(i,j)+c2(k))/msf(i,j)
          end do
       end do
    end do

commit 6f06c3670a6d930df752f8cee967f41a43972015

Fix a bug introduced in the af26bfb commit.
c1f should have been added in var/da/da_transfer_model/da_transfer_model.f90.

var/da/da_transfer_model/da_transfer_model.f90
@@ -42,7 +42,7 @@ module da_transfer_model
       its,ite,jts,jte,kts,kte, ips,ipe,jps,jpe,kps,kpe, qlimit, &
       update_sfcdiags, use_wrf_sfcinfo
    use da_control, only: base_pres_strat, base_lapse_strat
-   use da_control, only: c1h, c2h, c3f, c3h, c4f, c4h
+   use da_control, only: c1f, c2f, c1h, c2h, c3f, c3h, c4f, c4h
    use da_define_structures, only : xbx_type, be_type
    use da_par_util, only : da_patch_to_global
    use da_physics, only : da_check_rh_simple,da_roughness_from_lanu, &
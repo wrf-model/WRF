35e6d2fec9660f8b3a65045e9e2420b981cf06cf
Zhiquan (Jake) Liu <liuz@ucar.edu>
2018-12-17 14:33:26 -0700

    Add Himawari-8 AHI radiance DA capability (#731)
    TYPE: new feature
    
    KEYWORDS: Himawari-8, AHI, Radiance
    
    SOURCE: Yuanbing Wang (NUIST) and Jake Liu (NCAR)
    
    DESCRIPTION OF CHANGES:
    1. Himawari-8 AHI has 16 channels and this change allows the assimilation of 10 IR channels (channels 7-16).
    
    2. To make it work, need to do the following link under ~crtm_coeffs_2.3.0
    ln -sf ahi_himawari8.SpcCoeff.bin ahi_himawari08.SpcCoeff.bin
    ln -sf ahi_himawari8.TauCoeff.bin  ahi_himawari08.TauCoeff.bin
    
    3. Introduce a new namelist parameter 'varbc_scan(nsensor)' to choose either scan position (for polar-orbiting sensors, default varbc_scan=1) or satellite zenith angle (for GEO sensors, varbc_scan=2) as the angle predictor for variational bias correction. 
    
    4. It is assumed that netcdf format Himawari-8 AHI input files (both level1 and level2) are generated 
    from the raw HSD format data using CIMSS' GEOCAT software, which performs cloud detection.
    Input file names: L1AHITBR, L2AHICLP.
    
    5. NC file contains the full-disk data. The var/run/ahi_info file defines a sub-area to read.
    
    LIST OF MODIFIED FILES:
    M       Registry/registry.var
    M       var/build/depend.txt
    M       var/da/da_define_structures/da_define_structures.f90
    M       var/da/da_obs_io/da_read_y_unit.inc
    M       var/da/da_radiance/da_allocate_rad_iv.inc
    M       var/da/da_radiance/da_deallocate_radiance.inc
    M       var/da/da_radiance/da_initialize_rad_iv.inc
    A       var/da/da_radiance/da_qc_ahi.inc
    M       var/da/da_radiance/da_qc_rad.inc
    M       var/da/da_radiance/da_radiance.f90
    M       var/da/da_radiance/da_radiance1.f90
    M       var/da/da_radiance/da_radiance_init.inc
    A       var/da/da_radiance/da_read_obs_hdf5ahi.inc
    A       var/da/da_radiance/da_read_obs_netcdf4ahi_geocat.inc
    A       var/da/da_radiance/da_read_obs_netcdf4ahi_jaxa.inc
    M       var/da/da_radiance/da_setup_radiance_structures.inc
    M       var/da/da_radiance/module_radiance.f90
    M       var/da/da_setup_structures/da_setup_obs_structures.inc
    M       var/da/da_setup_structures/da_setup_structures.f90
    M       var/da/da_varbc/da_varbc.f90
    M       var/da/da_varbc/da_varbc_pred.inc
    M       var/run/VARBC.in
    A       var/run/ahi_info
    A       var/run/radiance_info/himawari-8-ahi.info
    
    TESTS CONDUCTED: 
    1. WRFDA-only changes. No impact on WRF.
    2. AHI radiance 3DVAR DA capability is tested at a 3-km resolution for a 2017061700 case over North China using CRTM.
    3. WRFDA regression tests passed with gnu and intel compilers.
    
    RELEASE NOTE: New capability for Himawari-8 AHI radiance data assimilation.
    Wang, Y., Z. Liu, S. Yang, J. Min, L. Chen, Y. Chen, and T. Zhang, 2018: Added value of assimilating Himawari-8 AHI water vapor radiances on analyses and forecasts for "7.19" severe storm over north China. J. Geophys. Res. Atmos., 123, https://doi.org/10.1002/2017JD027697.

--- var/da/da_radiance/da_radiance1.f90
+++ var/da/da_radiance/da_radiance1.f90
@@ -60,6 +60,8 @@ module da_radiance1
       real               ::   smois, tslb, snowh, elevation,soiltyp,vegtyp,vegfra
       real               ::   clw
       integer            ::   isflg
+      integer            ::   cloudflag
+
 !      real,    pointer   ::   tb_xb(:)
       real, pointer             :: tb_ob(:)
       real, pointer             :: tb_inv(:)
@@ -89,6 +91,7 @@ module da_radiance1
       real   ,  pointer  ::  ciw(:)   ! kg/kg
       real   ,  pointer  ::  rain(:)  ! kg/m2/s
       real   ,  pointer  ::  sp(:)    ! kg/m2/s
+     integer ,  pointer  ::  cloudflag(:)
    end type con_cld_vars_type
 
    type aux_vars_type
@@ -176,6 +179,7 @@ module da_radiance1
       real,    pointer   :: solidp(:)     ! solid precipitation rate in kg/m2/s
       real,    pointer   :: clw(:)        ! cloud liquid water (kg/kg)
       real,    pointer   :: ciw(:)        ! cloud ice water    (kg/kg)
+      integer, pointer   :: cloudflag(:)  ! cloud flag
 
    end type rad_data_type
 
@@ -233,6 +237,7 @@ contains
 #include "da_qc_atms.inc"
 #include "da_qc_seviri.inc"
 #include "da_qc_amsr2.inc"
+#include "da_qc_ahi.inc"
 #include "da_qc_goesimg.inc"
 #include "da_write_iv_rad_ascii.inc"
 #include "da_write_oa_rad_ascii.inc"

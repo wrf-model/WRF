31cbe3d8d8f56b1ac9cae85df42426ce60f69e77
Angel Farguell Caus <angel.farguell@gmail.com>
2019-02-18 16:04:46 -0700

    Using sh shell instead of csh shell

--- test/em_fire/create_links.sh
+++ test/em_fire/create_links.sh
@@ -1,4 +1,4 @@
-#!/bin/csh
+#!/bin/sh
 
 # Create directories with examples and link files there
 # This script is executed from compile em_fire
@@ -20,8 +20,10 @@ mkdir -p two_fires fmc rain
   ln -s ../input_sounding_rain input_sounding ; \
   ln -s ../namelist.fire_fmc namelist.fire ; \
   ln -s ../namelist.input_rain )
- foreach i (ETAMPNEW_DATA LANDUSE.TBL RRTMG_SW_DATA URBPARM.TBL \
-   GENPARM.TBL RRTMG_LW_DATA SOILPARM.TBL VEGPARM.TBL)
+ 
+for i in ETAMPNEW_DATA LANDUSE.TBL RRTMG_SW_DATA URBPARM.TBL GENPARM.TBL \
+RRTMG_LW_DATA SOILPARM.TBL VEGPARM.TBL
+do
    ( cd rain; ln -s ../../../run/$i .)
- end
+done
 

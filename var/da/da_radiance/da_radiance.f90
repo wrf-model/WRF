9558c8a4898255acbacbdda503f2d0998a8120bb
Wei Sun <weisun0416@gmail.com>
2019-02-25 14:18:32 -0700

    Add FY3C-MWHS2 radiance DA capability
    TYPE:new feature
    
    KEYWORDS: FY3C-MWHS2, Radiance, RTTOV
    
    SOURCE: Wei Sun (NSSC,CAS/NCAR) and Jake Liu (NCAR)
    
    DESCRIPITION OF CHANGES:
    Add new capability to assimilate FY3C-MWHS2 Level-1 data in HDF5 format, which can be downloaded from https://satellite.nsmc.org.cn/portalsite. Assimilation of this observation only works with RTTOV, since CRTM has no coefficients corresponding to FY3C.
    
    MODIFIED FILES:
    	modified:   Registry/registry.var
    	modified:   var/da/da_obs_io/da_read_y_unit.inc
    	new file:   var/da/da_radiance/da_qc_mwhs2.inc
    	modified:   var/da/da_radiance/da_qc_rad.inc
    	modified:   var/da/da_radiance/da_radiance.f90
    	modified:   var/da/da_radiance/da_radiance1.f90
    	modified:   var/da/da_radiance/da_radiance_init.inc
    	new file:   var/da/da_radiance/da_read_obs_hdf5mwhs2.inc
    	modified:   var/da/da_radiance/da_read_pseudo_rad.inc
    	modified:   var/da/da_radiance/da_setup_radiance_structures.inc
    	modified:   var/da/da_radiance/module_radiance.f90
    	modified:   var/da/da_setup_structures/da_setup_obs_structures.inc
    	modified:   var/da/da_setup_structures/da_setup_structures.f90
    	modified:   var/run/VARBC.in
    	new file:   var/run/radiance_info/fy3-3-mwhs2.info
    
    TEST CONDUCTED:
    1. WRFDA regression tests ran successfully with ifort+mpt and gnu+mpt on Cheyenne.
    2. New FY3C-MWHS2 radiance DA capability is tested on Cheyenne for 6-hour cycling DA over a 9km domain with a grid mesh of 600*450*50.

--- var/da/da_radiance/da_radiance.f90
+++ var/da/da_radiance/da_radiance.f90
@@ -46,7 +46,7 @@ module da_radiance
       tovs_min_transfer,use_error_factor_rad,num_fgat_time,stdout,trace_use, &
       qc_good, qc_bad,myproc,biascorr,thinning,thinning_mesh, &
       rad_monitoring, monitor_on, kts, kte, kms, kme, calc_weightfunc, &
-      use_mwtsobs, use_mwhsobs, use_atmsobs, use_amsr2obs, use_ahiobs, &
+      use_mwtsobs, use_mwhsobs, use_mwhs2obs, use_atmsobs, use_amsr2obs, use_ahiobs, &
       use_hirs4obs, use_mhsobs,bufr_year, bufr_month,bufr_day,bufr_hour, &
       bufr_minute, bufr_second,bufr_solzen, bufr_station_height, &
       bufr_landsea_mask,bufr_solazi,tovs_end, max_tovs_input, bufr_satzen, nchan_mhs, &
@@ -115,6 +115,7 @@ contains
 #include "da_write_filtered_rad.inc"
 #include "da_read_obs_bufrtovs.inc"
 #include "da_read_obs_fy3.inc"
+#include "da_read_obs_hdf5mwhs2.inc"
 #include "da_read_obs_bufratms.inc"
 #include "ATMS_Spatial_Average.inc"
 #include "da_read_obs_bufrairs.inc"

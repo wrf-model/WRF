7b7e0d461dacf01a158d86bccac47620bd5ee52c
DWesl <22566757+DWesl@users.noreply.github.com>
2019-10-02 07:06:43 -0400

    Changes to arch/Config.pl to support Cygwin.
    Combine two lines of perl to one, and allow the
    LIB_EXTERNAL=/LIB_BUNDLED= line to end with a backslash and still be
    processed sensibly.

--- arch/Config.pl
+++ arch/Config.pl
@@ -604,8 +604,7 @@ while ( <CONFIGURE_DEFAULTS> )
                  $_ .= " \$\(NETCDF4_IO_OPTS\)\n" ; 
                }
              if (/^LIB.*=/) 
-               { $_  =~ s/\r|\n//g ;
-                 $_ .=" \$\(NETCDF4_DEP_LIB\)\n" ;
+               { $_  =~ s/(\\?)[\r\n]/ \$\(NETCDF4_DEP_LIB\)\1\n/g ;
                }
            }
        }

c633384c7e86666c0a0bed9eac89eaaa8dd8098d
liujake <liuz@ucar.edu>
2020-11-27 20:35:02 -0700

    modified:   var/da/da_minimisation/da_minimisation.f90 	modified:   var/da/da_obs_io/da_search_obs.inc 	modified:   var/da/da_radiance/da_radiance1.f90 	modified:   var/da/da_radiance/da_read_iv_rad_for_multi_inc.inc

--- var/da/da_minimisation/da_minimisation.f90
+++ var/da/da_minimisation/da_minimisation.f90
@@ -33,7 +33,7 @@ module da_minimisation
    use da_buoy , only : da_calculate_grady_buoy, da_ao_stats_buoy, &
       da_oi_stats_buoy,da_get_innov_vector_buoy, da_residual_buoy, &
       da_jo_and_grady_buoy
-   use da_control, only : trace_use, var4d_bin, trajectory_io, analysis_date, qc_rad, &
+   use da_control, only : trace_use, var4d_bin, trajectory_io, analysis_date, &
       var4d, rootproc,jcdfi_use,jcdfi_diag,ierr,comm,num_fgat_time, &
       var4d_lbc, stdout, eps, stats_unit, test_dm_exact, global, multi_inc, &
       calculate_cg_cost_fn,anal_type_randomcv,cv_size_domain,je_factor, &
@@ -165,7 +165,7 @@ module da_minimisation
    use da_transfer_model, only : da_transfer_wrftltoxa,da_transfer_xatowrftl, &
       da_transfer_xatowrftl_adj,da_transfer_wrftltoxa_adj
 #if defined(RTTOV) || defined(CRTM)
-   use da_varbc, only : da_varbc_tl,da_varbc_adj,da_varbc_precond,da_varbc_coldstart,da_varbc_direct
+   use da_varbc, only : da_varbc_tl,da_varbc_adj,da_varbc_precond,da_varbc_coldstart, da_varbc_direct
 #endif
    use da_vtox_transforms, only : da_transform_vtox,da_transform_vtox_adj,da_transform_xtoxa,da_transform_xtoxa_adj
    use da_vtox_transforms, only : da_copy_xa, da_add_xa, da_transform_vpatox, da_transform_vpatox_adj
--- var/da/da_radiance/da_radiance1.f90
+++ var/da/da_radiance/da_radiance1.f90
@@ -10,10 +10,10 @@ module da_radiance1
    use module_radiance, only : CRTM_Planck_Radiance, CRTM_Planck_Temperature
 #endif
 #ifdef RTTOV
-   use module_radiance, only : coefs, coef_scatt
+   use module_radiance, only : coefs
 #endif
 
-   use da_control, only : trace_use,missing_r, rootproc, ierr,comm,root,&
+   use da_control, only : trace_use,missing_r, rootproc, &
       stdout,myproc,qc_good,num_fgat_time,qc_bad, &
       use_error_factor_rad,biasprep_unit,obs_qc_pointer, filename_len, &
       print_detail_rad, rtm_option, trace_use_dull, &
@@ -28,7 +28,7 @@ module da_radiance1
       be_type, clddet_geoir_type, superob_type
    use module_dm, only : wrf_dm_sum_real, wrf_dm_sum_integer
    use da_par_util, only : da_proc_stats_combine
-   use da_par_util1, only : da_proc_sum_int,da_proc_sum_ints,true_mpi_real
+   use da_par_util1, only : da_proc_sum_int,da_proc_sum_ints
    use da_reporting, only : da_error, message
    use da_statistics, only : da_stats_calculate
    use da_tools, only : da_residual_new, da_eof_decomposition
@@ -62,7 +62,6 @@ module da_radiance1
       real               ::   ps,ts,t2m,mr2m,u10,v10, clwp
       real               ::   smois, tslb, snowh, elevation,soiltyp,vegtyp,vegfra
       real               ::   clw
-      real               ::   tropt
       integer            ::   isflg
       integer            ::   cloudflag
 
@@ -83,14 +82,6 @@ module da_radiance1
       real   ,  pointer  ::  t_jac(:,:) => null()
       real   ,  pointer  ::  q_jac(:,:) => null()
       real   ,  pointer  ::  ps_jac(:)  => null()
-
-      real   ,  pointer  ::  ph(:)
-      real   ,  pointer  ::  cc(:)
-      real   ,  pointer  ::  clw(:)   ! kg/kg
-      real   ,  pointer  ::  ciw(:)   ! kg/kg
-      real   ,  pointer  ::  rain(:)  ! kg/kg
-      real   ,  pointer  ::  sp(:)    ! kg/kg
-
    end type con_vars_type
 
    type con_cld_vars_type
@@ -192,6 +183,7 @@ module da_radiance1
       real,    pointer   :: clw(:)        ! cloud liquid water (kg/kg)
       real,    pointer   :: ciw(:)        ! cloud ice water    (kg/kg)
       integer, pointer   :: cloudflag(:)  ! cloud flag
+
    end type rad_data_type
 
    type bias_type
@@ -218,7 +210,6 @@ module da_radiance1
    integer, allocatable :: tovs_recv_start(:,:)
    integer, allocatable :: tovs_copy_count(:)
 
-include 'mpif.h'
 contains
 
 #include "da_jo_and_grady_rad.inc"

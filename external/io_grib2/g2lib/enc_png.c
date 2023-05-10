808b12684449c675b8fdbcb08e65d681c9a5217d
Anthony Islas <aislas@ucar.edu>
2023-05-10 11:45:19 -0600

    I believe this was a bug, no idea how it was even working before

--- external/io_grib2/g2lib/enc_png.c
+++ external/io_grib2/g2lib/enc_png.c
@@ -88,7 +88,7 @@ int ENC_PNG(char *data,g2int *width,g2int *height,g2int *nbits,char *pngbuf)
 
 /*    Set new custom write functions    */
 
-    png_set_write_fn(png_ptr,(voidp)&write_io_ptr,(png_rw_ptr)user_write_data,
+    png_set_write_fn(png_ptr,(png_voidp)&write_io_ptr,(png_rw_ptr)user_write_data,
                     (png_flush_ptr)user_flush_data);
 /*    png_init_io(png_ptr, fptr);   */
 /*    png_set_compression_level(png_ptr, Z_BEST_COMPRESSION);  */

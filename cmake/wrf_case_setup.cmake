be1f37d11767f81809b02f0bfbf97eaa96b90440
Anthony Islas <aislas@ucar.edu>
2023-06-12 16:40:41 -0600

    Fixing link installs

--- cmake/wrf_case_setup.cmake
+++ cmake/wrf_case_setup.cmake
@@ -24,6 +24,7 @@ macro( wrf_link_targets )
                   message( STATUS \"Creating symlinks for $<TARGET_FILE_NAME:${WRF_LINK_TARGET}>\" )
                   execute_process( COMMAND ${CMAKE_COMMAND} -E create_symlink ${WRF_LINK_OLD_BASE_PATH}/$<TARGET_FILE_NAME:${WRF_LINK_TARGET}> ${WRF_LINK_SYM_PATH}/$<TARGET_FILE_NAME:${WRF_LINK_TARGET}> )
                   "
+            COMPONENT links
             )
 
   endforeach()
@@ -44,17 +45,20 @@ macro( wrf_link_files )
                         )
   
   foreach ( WRF_LINK_FILE ${WRF_LINK_FILES} )
-
+    
     # Generate install code for each file, this could be done in a simpler manner
     # with regular commands but to preserve order of operations it will be done via install( CODE ... )
     # https://stackoverflow.com/a/56528615
     get_filename_component( WRF_LINK_FULL_FILE ${WRF_LINK_FILE} REALPATH )
     get_filename_component( WRF_LINK_FILE_ONLY ${WRF_LINK_FILE} NAME     )
+    # Left here for debug purposes, may want to turn this into a trace-level debug msg
+    # message( "Generating install commands for ${WRF_LINK_FILE_ONLY} into ${WRF_LINK_SYM_PATH}" )
     install( 
             CODE "
                   message( STATUS \"Creating symlinks for ${WRF_LINK_FILE_ONLY}\" )
                   execute_process( COMMAND ${CMAKE_COMMAND} -E create_symlink ${WRF_LINK_FULL_FILE} ${WRF_LINK_SYM_PATH}/${WRF_LINK_FILE_ONLY} )
                   "
+            COMPONENT links            
             )
 
   endforeach()
@@ -74,21 +78,23 @@ macro( wrf_link_file_new_name )
                         ${ARGN}
                         )
   
-  foreach ( WRF_LINK_FILE ${WRF_LINK_FILES} )
-
-    # Generate install code for each file, this could be done in a simpler manner
-    # with regular commands but to preserve order of operations it will be done via install( CODE ... )
-    # https://stackoverflow.com/a/56528615
-    get_filename_component( WRF_LINK_FULL_FILE ${WRF_LINK_FILE} REALPATH )
-    get_filename_component( WRF_LINK_FILE_ONLY ${WRF_LINK_FILE} NAME     )
-    install( 
-            CODE "
-                  message( STATUS \"Creating symlinks for ${WRF_LINK_FILE_ONLY}\" )
-                  execute_process( COMMAND ${CMAKE_COMMAND} -E create_symlink ${WRF_LINK_FULL_FILE} ${WRF_LINK_SYM_PATH}/${WRF_LINK_FILE_ONLY} )
-                  "
-            )
-
-  endforeach()
+  
+  # Generate install code for each file, this could be done in a simpler manner
+  # with regular commands but to preserve order of operations it will be done via install( CODE ... )
+  # https://stackoverflow.com/a/56528615
+  get_filename_component( WRF_LINK_FULL_FILE ${WRF_LINK_FILE} REALPATH )
+  get_filename_component( WRF_LINK_FILE_ONLY ${WRF_LINK_FILE} NAME     )
+  get_filename_component( WRF_LINK_NEW_NAME_FULL_FILE ${WRF_LINK_NEW_NAME} REALPATH )
+  get_filename_component( WRF_LINK_NEW_NAME_FILE_ONLY ${WRF_LINK_NEW_NAME} NAME     )
+  # Left here for debug purposes, may want to turn this into a trace-level debug msg
+  # message( "Generating install commands for ${WRF_LINK_FILE_ONLY} to ${WRF_LINK_NEW_NAME_FILE_ONLY}" )
+  install( 
+          CODE "
+                message( STATUS \"Creating symlinks for ${WRF_LINK_FILE_ONLY} (rename ${WRF_LINK_NEW_NAME_FILE_ONLY})\" )
+                execute_process( COMMAND ${CMAKE_COMMAND} -E create_symlink ${WRF_LINK_FULL_FILE} ${WRF_LINK_NEW_NAME_FULL_FILE} )
+                "
+          COMPONENT links
+          )
 
 endmacro()
 

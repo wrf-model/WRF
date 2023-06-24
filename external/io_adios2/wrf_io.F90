c66b531655fe4662eb925ea345d9672feab87d38
Anthony Islas <aislas@ucar.edu>
2023-06-23 20:08:21 -0600

    Reinstating old c preproc then m4 processing since gfortran is unable to

--- external/io_adios2/wrf_io.F90
+++ external/io_adios2/wrf_io.F90
@@ -702,9 +702,9 @@ module ext_adios2_support_routines
  
    call LowerCase(MemoryOrder,MemOrd)
    select case (MemOrd)
- 
- !#define XDEX(A,B,C) A-A ## 1+1+(A ## 2-A ## 1+1)*((B-B ## 1)+(C-C ## 1)*(B ## 2-B ## 1+1))
- ! define(`XDEX',($1-``$1''1+1+(``$1''2-``$1''1+1)*(($2-``$2''1)+($3-``$3''1)*(``$2''2-``$2''1+1))))
+! Cannot use following define due to gfortran cpp traditional mode concatenation limitations 
+!#define XDEX(A,B,C) A-A ## 1+1+(A ## 2-A ## 1+1)*((B-B ## 1)+(C-C ## 1)*(B ## 2-B ## 1+1))
+! define(`XDEX',($1-``$1''1+1+(``$1''2-``$1''1+1)*(($2-``$2''1)+($3-``$3''1)*(``$2''2-``$2''1+1))))
      case ('xzy')
 #undef  DFIELD
 #define DFIELD XField(1:di,XDEX(i,k,j))

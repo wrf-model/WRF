4732df17b94bd786ea19ef8d72c412e54099f944
Anthony Islas <aislas@ucar.edu>
2023-08-09 15:47:38 -0600

    Example test infrastructure

---
+++ .ci/tests/build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+workingDirectory=$1
+shift
+compileOption=$1
+makejobs=$3
+
+cd $workingDirectory
+
+echo -ne "$compileOption\n" | ./configure
+
+if [ ! -f configure.wrf ]; then
+  echo  "Failed to configure"
+  exit 1
+fi
+
+./compile em_real $makejobs
+
+result=$?
+
+if [ $result -eq 0 ]; then
+  echo "TEST $(basename $0) PASS"
+fi

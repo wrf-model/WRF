39513f1c66bac6b1fb76e4c4afff45c236eb8e55
Anthony Islas <aislas@ucar.edu>
2023-10-02 15:40:41 -0700

    Use exact same version as scala tests

--- .ci/env/wsl.sh
+++ .ci/env/wsl.sh
@@ -3,8 +3,8 @@
 echo "Setting up WSL environment"
 
 export NETCDF=/opt/ncar/netcdf
-export MPICH=/opt/ncar/mpich
-export PATH=$PATH:$NETCDF/bin
+export MPI=/opt/ncar/openmpi-4.0.0
+export PATH=$MPI/bin/:$NETCDF/bin:$PATH:
 
 # use python virtaul env
 . /opt/ncar/netcdf-python/bin/activate

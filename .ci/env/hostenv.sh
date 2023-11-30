d7eebb092c7befbbe8acbde5437f7e416b819b0d
Anthony Islas <aislas@ucar.edu>
2023-11-30 11:44:27 -0700

    Adding correct derecho configuration and sourcing

--- .ci/env/hostenv.sh
+++ .ci/env/hostenv.sh
@@ -9,6 +9,9 @@ if [ $( contains ${hostname} polaris ) -eq 0 ]; then
 elif [ $( contains ${hostname} cheyenne ) -eq 0 ]; then
   # Cheyenne HPC SuSE PBS
   . .ci/env/cheyenne.sh
+elif [ $( contains ${hostname} hsn.de.hpc ) -eq 0 ]; then
+  # Derecho HPC SuSE PBS
+  . .ci/env/derecho.sh
 else
   echo "No known environment for '${hostname}', using current"
 fi

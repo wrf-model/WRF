f65b612c4e7bef4ccd08cb5e1f6d695aa54de204
Anthony Islas <aislas@ucar.edu>
2024-01-02 12:30:27 -0700

    Full support for out-of-source build and installs

--- cmake/wrf_case_setup.cmake
+++ cmake/wrf_case_setup.cmake
@@ -1,64 +1,72 @@
-# WRF Macro for adding target symlinks to be run after internal install() code
-macro( wrf_link_targets )
+# WRF Macro for adding target symlinks/copies to be run after internal install() code
+macro( wrf_setup_targets )
 
-  set( options        )
-  set( oneValueArgs   SYM_PATH OLD_BASE_PATH )
+  set( options        USE_SYMLINKS )
+  set( oneValueArgs   DEST_PATH OLD_BASE_PATH )
   set( multiValueArgs TARGETS  )
 
   cmake_parse_arguments(
-                        WRF_LINK
+                        WRF_SETUP
                         "${options}"  "${oneValueArgs}"  "${multiValueArgs}"
                         ${ARGN}
                         )
+  set( WRF_SETUP_CMD copy_if_different )
+  if ( ${WRF_SETUP_USE_SYMLINKS} )
+    set( WRF_SETUP_CMD create_symlink )
+  endif()
 
 
-  foreach ( WRF_LINK_TARGET ${WRF_LINK_TARGETS} )
+  foreach ( WRF_SETUP_TARGET ${WRF_SETUP_TARGETS} )
 
     # Generate install code for each target
     # https://stackoverflow.com/a/56528615
     #!TODO Do we *need* the rm for symlinks beforehand?
-    # get_filename_component( WRF_LINK_FILE_ONLY $<TARGET_FILE:${WRF_LINK_TARGET}> NAME
+    # get_filename_component( WRF_SETUP_FILE_ONLY $<TARGET_FILE:${WRF_SETUP_TARGET}> NAME
 
     install( 
             CODE "
-                  message( STATUS \"Creating symlinks for $<TARGET_FILE_NAME:${WRF_LINK_TARGET}>\" )
-                  execute_process( COMMAND ${CMAKE_COMMAND} -E create_symlink ${WRF_LINK_OLD_BASE_PATH}/$<TARGET_FILE_NAME:${WRF_LINK_TARGET}> ${WRF_LINK_SYM_PATH}/$<TARGET_FILE_NAME:${WRF_LINK_TARGET}> )
+                  message( STATUS \"Setting up $<TARGET_FILE_NAME:${WRF_SETUP_TARGET}> via ${WRF_SETUP_CMD}\" )
+                  execute_process( COMMAND ${CMAKE_COMMAND} -E ${WRF_SETUP_CMD} ${WRF_SETUP_OLD_BASE_PATH}/$<TARGET_FILE_NAME:${WRF_SETUP_TARGET}> ${WRF_SETUP_DEST_PATH}/$<TARGET_FILE_NAME:${WRF_SETUP_TARGET}> )
                   "
-            COMPONENT links
+            COMPONENT setup
             )
 
   endforeach()
 
 endmacro()
 
-# WRF Macro for adding file symlinks to be run after internal install() code
-macro( wrf_link_files )
+# WRF Macro for adding file symlinks/copies to be run after internal install() code
+macro( wrf_setup_files )
 
-  set( options        )
-  set( oneValueArgs   SYM_PATH )
+  set( options        USE_SYMLINKS )
+  set( oneValueArgs   DEST_PATH )
   set( multiValueArgs FILES  )
 
   cmake_parse_arguments(
-                        WRF_LINK
+                        WRF_SETUP
                         "${options}"  "${oneValueArgs}"  "${multiValueArgs}"
                         ${ARGN}
                         )
+  set( WRF_SETUP_CMD copy_if_different )
+  if ( ${WRF_SETUP_USE_SYMLINKS} )
+    set( WRF_SETUP_CMD create_symlink )
+  endif()
   
-  foreach ( WRF_LINK_FILE ${WRF_LINK_FILES} )
+  foreach ( WRF_SETUP_FILE ${WRF_SETUP_FILES} )
     
     # Generate install code for each file, this could be done in a simpler manner
     # with regular commands but to preserve order of operations it will be done via install( CODE ... )
     # https://stackoverflow.com/a/56528615
-    get_filename_component( WRF_LINK_FULL_FILE ${WRF_LINK_FILE} ABSOLUTE )
-    get_filename_component( WRF_LINK_FILE_ONLY ${WRF_LINK_FILE} NAME     )
+    get_filename_component( WRF_SETUP_FULL_FILE ${WRF_SETUP_FILE} ABSOLUTE )
+    get_filename_component( WRF_SETUP_FILE_ONLY ${WRF_SETUP_FILE} NAME     )
     # Left here for debug purposes, may want to turn this into a trace-level debug msg
-    # message( "Generating install commands for ${WRF_LINK_FILE_ONLY} into ${WRF_LINK_SYM_PATH}" )
+    # message( "Generating install commands for ${WRF_SETUP_FILE_ONLY} into ${WRF_SETUP_DEST_PATH}" )
     install( 
             CODE "
-                  message( STATUS \"Creating symlinks for ${WRF_LINK_FILE_ONLY}\" )
-                  execute_process( COMMAND ${CMAKE_COMMAND} -E create_symlink ${WRF_LINK_FULL_FILE} ${WRF_LINK_SYM_PATH}/${WRF_LINK_FILE_ONLY} )
+                  message( STATUS \"Setting up ${WRF_SETUP_FILE_ONLY} via ${WRF_SETUP_CMD}\" )
+                  execute_process( COMMAND ${CMAKE_COMMAND} -E ${WRF_SETUP_CMD} ${WRF_SETUP_FULL_FILE} ${WRF_SETUP_DEST_PATH}/${WRF_SETUP_FILE_ONLY} )
                   "
-            COMPONENT links            
+            COMPONENT setup
             )
 
   endforeach()
@@ -66,34 +74,38 @@ macro( wrf_link_files )
 endmacro()
 
 # WRF Macro for adding file symlink to be run after internal install() code
-macro( wrf_link_file_new_name )
+macro( wrf_setup_file_new_name )
 
-  set( options        )
-  set( oneValueArgs   SYM_PATH FILE NEW_NAME )
+  set( options        USE_SYMLINKS )
+  set( oneValueArgs   FILE NEW_NAME )
   set( multiValueArgs )
 
   cmake_parse_arguments(
-                        WRF_LINK
+                        WRF_SETUP
                         "${options}"  "${oneValueArgs}"  "${multiValueArgs}"
                         ${ARGN}
                         )
   
+  set( WRF_SETUP_CMD copy_if_different )
+  if ( ${WRF_SETUP_USE_SYMLINKS} )
+    set( WRF_SETUP_CMD create_symlink )
+  endif()
   
   # Generate install code for each file, this could be done in a simpler manner
   # with regular commands but to preserve order of operations it will be done via install( CODE ... )
   # https://stackoverflow.com/a/56528615
-  get_filename_component( WRF_LINK_FULL_FILE ${WRF_LINK_FILE} ABSOLUTE )
-  get_filename_component( WRF_LINK_FILE_ONLY ${WRF_LINK_FILE} NAME     )
-  get_filename_component( WRF_LINK_NEW_NAME_FULL_FILE ${WRF_LINK_NEW_NAME} ABSOLUTE )
-  get_filename_component( WRF_LINK_NEW_NAME_FILE_ONLY ${WRF_LINK_NEW_NAME} NAME     )
+  get_filename_component( WRF_SETUP_FULL_FILE ${WRF_SETUP_FILE} ABSOLUTE )
+  get_filename_component( WRF_SETUP_FILE_ONLY ${WRF_SETUP_FILE} NAME     )
+  get_filename_component( WRF_SETUP_NEW_NAME_FULL_FILE ${WRF_SETUP_NEW_NAME} ABSOLUTE )
+  get_filename_component( WRF_SETUP_NEW_NAME_FILE_ONLY ${WRF_SETUP_NEW_NAME} NAME     )
   # Left here for debug purposes, may want to turn this into a trace-level debug msg
-  # message( "Generating install commands for ${WRF_LINK_FILE_ONLY} to ${WRF_LINK_NEW_NAME_FILE_ONLY}" )
+  # message( "Generating install commands for ${WRF_SETUP_FILE_ONLY} to ${WRF_SETUP_NEW_NAME_FILE_ONLY}" )
   install( 
           CODE "
-                message( STATUS \"Creating symlinks for ${WRF_LINK_FILE_ONLY} (rename ${WRF_LINK_NEW_NAME_FILE_ONLY})\" )
-                execute_process( COMMAND ${CMAKE_COMMAND} -E create_symlink ${WRF_LINK_FULL_FILE} ${WRF_LINK_NEW_NAME_FULL_FILE} )
+                message( STATUS \"Setting up ${WRF_SETUP_FILE_ONLY} (rename ${WRF_SETUP_NEW_NAME_FILE_ONLY}) via ${WRF_SETUP_CMD}\" )
+                execute_process( COMMAND ${CMAKE_COMMAND} -E ${WRF_SETUP_CMD} ${WRF_SETUP_FULL_FILE} ${WRF_SETUP_NEW_NAME_FULL_FILE} )
                 "
-          COMPONENT links
+          COMPONENT setup
           )
 
 endmacro()

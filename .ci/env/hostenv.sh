3682cf7ac01a69a20c56f59cd608e5173dab494c
Anthony Islas <aislas@ucar.edu>
2024-03-05 15:52:40 -0700

    Add mmm-mystic config

--- .ci/env/hostenv.sh
+++ .ci/env/hostenv.sh
@@ -6,6 +6,9 @@ hostname=$( python3 -c "import socket; print( socket.getfqdn() )" )
 if [ $( contains ${hostname} polaris ) -eq 0 ]; then
   # WSL Agent
   . .ci/env/wsl.sh $*
+elif [ $( contains ${hostname} mmm-mystic ) -eq 0 ]; then
+  # Personal asset
+  . .ci/env/mystic.sh $*
 elif [ $( contains ${hostname} cheyenne ) -eq 0 ]; then
   # Cheyenne HPC SuSE PBS
   . .ci/env/cheyenne.sh

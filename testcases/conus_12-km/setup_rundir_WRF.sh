a14a1730e4ba7a802495a5a3a520ea635b41cfe0
Woo-Sun Yang <wyang@lbl.gov>
2024-07-01 17:57:54 -0700

    setup_rundir_WRF.sh now contains input files for SBM prepared by Namo

--- testcases/conus_12-km/setup_rundir_WRF.sh
+++ testcases/conus_12-km/setup_rundir_WRF.sh
@@ -46,3 +46,4 @@ cp ${scriptdir}/sub_wrf_pm_testcase.sh ${rundir}/sub_testcase.sh
 sed -i "s/elvis@nersc.gov/${myemail}/" sub_testcase.sh
 sed -i "s/elvis_test01/${casename}/" sub_testcase.sh
 
+sbatch sub_testcase.sh
